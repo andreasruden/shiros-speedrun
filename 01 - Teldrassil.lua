a5ec6d926c9b76ccb8067f8445c3619b021aa209
Andreas Ruden <andreasruden91@gmail.com>
2021-07-02 15:27:27 +0200

    Start on Teldrassil

---
+++ 01 - Teldrassil.lua
@@ -0,0 +1,48 @@
+Guidelime.registerGuide(
+[[
+
+[D Shiros 1-58 Speedrun for alliance (patch 2.3+)]
+[GA Alliance]
+[N 1-12 Teldrassil]
+[NX 13-15 TBD]
+
+*Some speedrun tips*:
+*Speedrunning is best done with a class that can kill while moving*. Always try to strafe towards your next objective while killing. And always look to kill/grind while headed somewhere.
+
+Accept [QA456 The Balance of Nature].
+Accept [QA458 The Woodland Protector].
+Turn in [QT458 The Woodland Protector].\\Accept [QT459 The Woodland Protector]
+Complete [QA456 The Balance of Nature] and [QA459 The Woodland Protector].
+Turn in [QA459 The Woodland Protector].
+Turn in [QA456 The Balance of Nature].\\Accept [QA457 The Balance of Nature].
+Accept [QA4495 A Good Friend].
+[OC][V]Vendor at [G59.3,41,1 Keina].
+Accept [QA916 Webwood Venom].
+[O]During the next steps, kill any *Thistleboar*, *Mangy Nightsaber*, and *Webwood Spider*(XXX) that you come across for [QC457] and [QC916].
+Talk to Iverron to turn in [QT4495 A Good Friend].\\Accept [QA3519 A Friend in Need].
+XXX: Die or walk?
+Turn in [QT3519 A Friend in Need].\\Accept [QA3521 Iverron's Antidote].
+Collect [QC3521,1 Hyacinth Mushroom] and [QC3521,2 Moonpetal Lily].
+Complete [QC457 The Balance of Nature] by killing mobs around [G60.3,34.7].
+Kill spiders outside the cave for the [QC3521,3 Webwood Ichor].
+Complete [QC916 Webwood Venom].
+Die & spirit ress.
+Turn in [QT457 The Balance of Nature].
+[A Rogue]Accept [QA3118 Encrypted Sigil].
+[A Rogue]Turn in [QC3118 Encrypted Sigil] and [T] train level 2 & 4 skills.
+-- Note: Classes that don't go up the tree for their skills should grab it after balance of nature last part.
+[OC][V][R]Vendor at [G59.3,41,1 Keina].
+Turn in [QT3521 Iverron's Antidote].\\Accept [QA3522 Iverron's Antidote].
+Turn in [QT916 Webwood Venom].\\Accept [QA917 Webwood Egg].
+Turn in [QC3522 Iverron's Antidote].
+Complete [QC917 Webwood Egg].
+Die & Spirit ress.
+Turn in [QT917 Webwood Egg].\\Accept [QA920 Tenaron's Summons].
+Turn in [QT920 Tenaron's Summons].\\Accept [QA921 Crown of the Earth].
+-- Note: Classes that go up the tree for their skills should grab it together with Tenaron turnin.
+Jump straight away from Tenaron. Then run to [G61.2, 47.4].\\Accept [QA2159 Dolanaar Delivery].
+-- End of Shadowglen
+
+XXX: Better to die or run here?
+
+]], 'Shiros Speedrun')
