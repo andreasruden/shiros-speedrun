3bc5d4aa31efa292a9d0b1a8127854129c8870df
Andreas Ruden <andreasruden91@gmail.com>
2021-07-02 16:35:47 +0200

    Start on Dolanaar

--- 01 - Teldrassil.lua
+++ 01 - Teldrassil.lua
@@ -40,9 +40,56 @@ Die & Spirit ress.
 Turn in [QT917 Webwood Egg].\\Accept [QA920 Tenaron's Summons].
 Turn in [QT920 Tenaron's Summons].\\Accept [QA921 Crown of the Earth].
 -- Note: Classes that go up the tree for their skills should grab it together with Tenaron turnin.
-Jump straight away from Tenaron. Then run to [G61.2, 47.4].\\Accept [QA2159 Dolanaar Delivery].
+Jump straight away from Tenaron. Then run to [G61.2,47.4].\\Accept [QA2159 Dolanaar Delivery].
 -- End of Shadowglen
 
-XXX: Better to die or run here?
+-- Arriving in Dolanaar
+Accept [QA488 Zenn's Bidding]. Note: go behind the tree, don't follow the road.
+Accept [QA997 Denalan's Earth].
+Accept [QA475 A Troubling Breeze].
+Learn *First Aid* from Byancie [G55.3,56.8].
+Accept [QA932 Twisted Hatred] and [QA2438 The Emerald Dreamcatcher].
+Turn in [QC2159 Dolanaar Delivery]. Set hearthstone in [S Dolanaar].
+
+-- First circle of eastern Teldrassil.
+[O] During the next steps kill *Nightsaber*, *Webwood Lurker*, and *Strigid Owl* as you encounter them, for [QC488].
+[G59.6,61.4]
+[G63.2,63.7]
+Turn in [QC997 Denalan's Earth].
+Accept [QA918 Timberling Seeds] and [QA919 Timberling Sprouts].
+Complete [QC919 Timberling Sprouts].
+Complete [QC918 Timberling Seeds].
+Turn in [QT918 Timberling Seeds] and [QT919 Timberling Sprouts].
+Accept [QA922 Rellian Greenspyre].
+[G62.7,72.3]
+[G65.3,66.9]
+[G68.2,63.3]
+Complete [QC2438 The Emerald Dreamcatcher].
+Turn in [QT475 A Troubling Breeze].\\Accept [QA476 Gnarlpine Corruption].
+If not already completed, complete [QC488 Zenn's Bidding] on your way back to Zenn (the Satyr).
+Turn in [QT488 Zenn's Bidding].
+Accept [QA489 Seek Redemption!].
+Turn in [QT476 Gnarlpine Corruption].\\Accept [QA483 The Relics of Wakening].
+[V] Vendor and [T] train level 6 skills.
+Enter the [G 54.7,52.6 Fel Rock cave] behind Dolanaar.
+Keep to the left as you advance in the cave. Then kill Lord Melenas for [QC932 Twisted Hatred].
+XXX: Die to go back? On priv servers this puts you in shadowglen.
+[OC] On the way up the tree, [V] buy 6 slot bags at Aldia if you have silver over. (4s 75c each)
+Turn in [QT932 Twisted Hatred] and [QT2438 The Emerald Dreamcatcher].\\Accept [QA2459 Ferocitas the Dream Eater].
+
+-- Second circle of eastern Teldrassil.
+
+-- Listing of Teldrassil Quests:
+-- Initial Dolanaar Quests: 488 Zenn's Bidding (Satyr), Outside tree building: 997 Denalan's Earth, 475 A Troubling Breeze,
+-- Top of Tree: 932 Twisted Hatred, 2438 The Emerald Dreamcatcher, From Moon Priestess patrol: 487 The Road to Darnassus
+--   A Troubling Breeze (4 quests) -> Starbreeze, back to Dolanaar, inside Barrow's Den (Oben /w Sleeping Druid in here), kill Ursal in bottom left corner.
+--   Denalan's Earth: Leads to:
+--   .. Timberling Seeds (5 quests) -> seeds around denalan, delivery to darnassus, tumors in NW lake, back to darnassus (turn-in), back to denalan, kill Oakenscowl (south of lake al'ameth)
+--   .. Timberling Sprouts: Done together with the seeds.
+--   Zenn's Bidding (2 quests) -> kill wildlife, then collect Fel Cone (for Seek Redemption!). Both done all around the southern part.
+--   The Emerald Dreamcatcher (2 quests) -> loot in Starbreeze, turn in in Dolanaar, kill Ferocitas (N of starbreeze), back to Dolanaar
+--   Twisted Hatred: Cave north of Dolanaar.
+--   The Glowing Fruit: After timberling sprout/seeds, can loot in SW. Turn in at Denalan.
+--   The Shimmering Frond: Can loot in NW after XXX. Turn in at Denalan. Leads to The Sprouted Fronds (instant reward).
 
 ]], 'Shiros Speedrun')
