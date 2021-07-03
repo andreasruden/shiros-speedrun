5d180d70e6dbea556492d3ba1a355d65822b28c0
Andreas Ruden <andreasruden91@gmail.com>
2021-07-03 07:29:14 +0200

    Finish with quests in Teldrassil

--- 01 - Teldrassil.lua
+++ 01 - Teldrassil.lua
@@ -40,6 +40,8 @@ Die & Spirit ress.
 Turn in [QT917 Webwood Egg].\\Accept [QA920 Tenaron's Summons].
 Turn in [QT920 Tenaron's Summons].\\Accept [QA921 Crown of the Earth].
 -- Note: Classes that go up the tree for their skills should grab it together with Tenaron turnin.
+Jump straight away from Tenaron, and Complete [QC921 Crown of the Earth] at the moonwell you can see to your left.
+Turn in [QT921 Crown of the Earth] back at Tenaron.\\Accept follow-up [QA928 Crown of the Earth].
 Jump straight away from Tenaron. Then run to [G61.2,47.4].\\Accept [QA2159 Dolanaar Delivery].
 -- End of Shadowglen
 
@@ -50,8 +52,10 @@ Accept [QA475 A Troubling Breeze].
 Learn *First Aid* from Byancie [G55.3,56.8].
 Accept [QA932 Twisted Hatred] and [QA2438 The Emerald Dreamcatcher].
 Turn in [QC2159 Dolanaar Delivery]. Set hearthstone in [S Dolanaar].
+Turn in [QT928 Crown of the Earth].\\Accept follow-up [QA929 Crown of the Earth].
+XXX: Learn Cooking here? Zarrin is right next to us. + Level it to 10? or 40? Probably can't afford that many spices here.
 
--- First circle of eastern Teldrassil.
+-- Go east for First batch of quests.
 [O] During the next steps kill *Nightsaber*, *Webwood Lurker*, and *Strigid Owl* as you encounter them, for [QC488].
 [G59.6,61.4]
 [G63.2,63.7]
@@ -66,6 +70,7 @@ Accept [QA922 Rellian Greenspyre].
 [G68.2,63.3]
 Complete [QC2438 The Emerald Dreamcatcher].
 Turn in [QT475 A Troubling Breeze].\\Accept [QA476 Gnarlpine Corruption].
+Go to [G53.4,58.2 Moonwell] and complete [QC929 Crown of the Earth].
 If not already completed, complete [QC488 Zenn's Bidding] on your way back to Zenn (the Satyr).
 Turn in [QT488 Zenn's Bidding].
 Accept [QA489 Seek Redemption!].
@@ -74,15 +79,117 @@ Turn in [QT476 Gnarlpine Corruption].\\Accept [QA483 The Relics of Wakening].
 Enter the [G 54.7,52.6 Fel Rock cave] behind Dolanaar.
 Keep to the left as you advance in the cave. Then kill Lord Melenas for [QC932 Twisted Hatred].
 XXX: Die to go back? On priv servers this puts you in shadowglen.
-[OC] On the way up the tree, [V] buy 6 slot bags at Aldia if you have silver over. (4s 75c each)
+Half-way up the tree, [V] buy a *Skinning Knife* from Narret.
+[OC] Right next to Narret, [V] buy 6 slot bags at Aldia if you have silver over. (4s 75c each)
 Turn in [QT932 Twisted Hatred] and [QT2438 The Emerald Dreamcatcher].\\Accept [QA2459 Ferocitas the Dream Eater].
+Turn in [QT929 Crown of the Earth].\\Accept [QA933 Crown of the Earth].
 
--- Second circle of eastern Teldrassil.
+-- Kill Ferocitas. XXX: Is there any other reason to go east again? Or do we have to go for just Ferocitas?
+[G58.5,56.4] 3 Fel Cones can spawn here.
+[OC] Try to complete [QC489 Seek Redemption!] on the way to the next objective.
+[G69.4,53.4 Kill Ferorcitias] and loot the Necklace. Open it to get [QC2459,2 Tallonkai's Jewel].
+Kill Mystics to finish [QC2459 Ferocitas the Dream Eater].
+Grind until [XP8-1510].
+[OC] If Seek Redemption! was not completed grind until [XP8-880].
+Die & Spirit ress. (XXX: This is shadowglen on priv servers)
+Turn in [QT2459 Fermocitas the Dream Eater].
+[OC] If completed, turn in [QT489 Seek Redemption!].
+[T] Learn level 8 skills.
+
+-- Barrow Den!
+Accept [QA487 The Road to Darnassus] from Moon Priestess Amara, who patrols between Dolanaar and (48,51).
+[G42.1,50.05] Learn *Skinning* from Radnaal.
+[OC] If not completed, complete [QC489 Seek Redemption!] while doing the next quests.
+[OC] Note: On the way to the next quests, kill beasts & critters for skinning.
+[G40.35,54.05]
+[G40.77,61.98]
+At the [G42.38,67.10 Moonwell] fill your flask for [QC933 Crown of the Earth].
+[OC] Note: It's a good idea to do some extra nightsaber & critter skinning around here, or you might end up waiting for respawns later.
+Backtrack to complete [Q487 The Road to Darnassus].
+Go to [G44.5,57.7 Barrow Den].
+Follow the tunnel until you reach the [G43.6, 60.0 two bridges].
+Loot [QC483,2 Black Feather Quill] from the right bridge.
+Loot [QC483,4 Rune of Nesting] from the left bridge.
+[G44, G59.6] Continue down.
+[G43.3,60.7]
+[G43.83,61.75]
+Across the bridge, accept the quest [QA2541 The Sleeping Druid].
+Continue down and get [QC483,3 Sapphire of Sky].
+[OC] Note: If at any point you get the *Shaman Voodoo Charm* for [QC2541 The Sleeping Druid], go back and turn it in.
+[G44.33,60.31] Follow the tunnel.
+[G45.53,58.90] Go up, avoid aggroing *Rageclaw*.
+[G45.21,58.74]
+[G45.35,58.05]
+[G46.17,58.38] Go right.
+Follow the tunnel and loot [QC483,1 Raven Claw Talisman].
+Complete [QC2541 The Sleeping Druid] by skilling *Gnarlpine Shaman*.
+Turn in [QT2541 The Sleeping Druid] back at Oben Rageclaw.\\Accept [QA2561 Druid of the Claw].
+[G44.33,60.31] Follow the tunnel again.
+[G45.53,58.90]
+Kill *Rageclaw*, then use the *Voodoo Charm* to complete [QC2561 Druid of the Claw].
+Turn in [QT2561 Druid of the Claw] back at Oben Rageclaw.
+-- Grinding for some classes (1050 for Druid of the Claw, 780 for Crown of the Earth, 780 for Relics, 700 for road)
+-- XXX: What other classes need to grind?
+[A Rogue]Grind to [XP10-3310] (630 less if you've not turned in *Seek Redemption!* yet).
+Die & ress.
+Turn in [QT933 Crown of the Earth].\\Accept follow-up [QA934 Crown of the Earth].
+Turn in [QT2561 Druid of the Claw].
+Turn in [QT483 The Relics of Wakening].\\Accept [QA486 Ursal the Mauler].
+[OC] If not already turned in, turn in [QT489 Seek Redemption!].
+Turn in [QT487 The Road to Darnassus].
+[A Rogue]Accept [QA2241 The Apple Falls] and [T] learn skills.
+
+-- Darnassus & Western-side quests
+Head west to [G36.06,54.35 Darnassus]. Try to get some skinning skills on the way.
+Turn in [QT922 Rellian Greenspyre].\\Accept [QA923 Tumors].
+[A Rogue] Walk under the tree and turn in [QT2241 The Apple Falls].\\Accept [QA2242 Destiny Calls].
+Accept [QA2519 The Temple of the Moon].
+Turn in [QT2519 The Temple of the Moon].\\Accept the follow-up [QA2518 Tears of the Moon].
+[OC][V][R] Vendor & repair at [G52.92,80.07 Cyridan] on the way out of Darnassus.
+[OC] Note: During the next quests, kill beasts/critters & skin when you get the chance. We're soon going to need *skinning 65* to skin level 13 mobs.
+Accept [QA937 The Enchanted Glade].
+Fill the vial in the Moonwell right behind Arynia to complete [QC934 Crown of the Earth].
+Go east to the Wellspring River and gather *Mossy Tumor* to complete [QC923 Tumors].
+Kill Lady Sathrah, who can be NE or NW of Wellspring Lake, to complete [QC2518 Tears of the Moon].
+Head to [G34.82, 28.91 Strange Fronded Plant], killing harpies and skinning on the way.
+[A Rogue] [G37.51,24.06]
+[A Rogue] Use *Pick Pocket* on Sethir and complete [QC2242 Destiny Calls]. If you aggro him he spawns a bunch of minions, so make sure to approach from behind. He patrols around the branch.
+Accept [QA931 The Shimmering Frond].
+Murder you way over to [G31.9,32.2 Mist].
+Accept [QA938 Mist].
+Complete [QC937 The Enchanted Glade], if you still don't have all 6.
+Escort Mist to Arynia to complete [QC938 Mist].
+Turn in [QT937 The Enchanted Glade] and [QT938 Mist].\\Accept [QA940 Teldrassil].
+Turn in [QT923 Tumors].\\Accept [QA2498 Return to Denalan].
+[A Rogue] Turn in [QT2242 Destiny Calls].
+Turn in [QT2518 Tears of the Moon].\\Accept [QA2520 Sathrah's Sacrifice].
+On the bottom floor in the fountain use *Sathrah's Sacrifice* to complete [QC2520 Sathrah's Sacrifice].
+Turn in [QT2520 Sathrah's Sacrifice].
+Level skinning to 55 between east and south-east of Darnassus [G43.8,73.5]. (Skin nightsabers & critters)
+Accept [QA930 The Glowing Fruit].
+Complete [QC486 Ursal the Mauler].
+Hearthstone to [H Dolanaar].
+
+-- Finishing up Teldrassil
+Turn in [QT486 Ursal the Mauler].
+Turn in [QT934 Crown of the Earth].\\Accept follow-up [QA935 Crown of the Earth].
+Turn in [QT930 The Glowing Fruit], [QT2498 Return to Denalan] and [QT931 The Shimmering Frond].
+Accept [QA2399 The Sprouted Fronds] and turn in [QT2399 The Sprouted Fronds].
+Accept [QA2499 Oakenscowl].
+Complete [QC2499 Oakenscowl].
+Turn in [QT2499 Oakenscowl].
+[G42.1,50.05] Learn *Journeyman Skinning* from Radnaal.
+Accept [QA6344 Nessa Shadowsong].
+Turn in [QT940 Teldrassil] and [QT935 Crown of the Earth] up the tree.\\Accept [QA952 Grove of the Ancients].
+[T] If you're level 12 learn skills. Might also want to pick up weapon specializations from Darnassus.
+Turn in [QT6344 Nessa Shadowsong].\\Accept [QA6341 The Bounty of Teldrassil].
+Turn in [QT6341 The Bounty of Teldrassil].\\Accept [QA6342 Flight to Auberdine].
+Fly to [F Auberdine].
 
 -- Listing of Teldrassil Quests:
 -- Initial Dolanaar Quests: 488 Zenn's Bidding (Satyr), Outside tree building: 997 Denalan's Earth, 475 A Troubling Breeze,
 -- Top of Tree: 932 Twisted Hatred, 2438 The Emerald Dreamcatcher, From Moon Priestess patrol: 487 The Road to Darnassus
---   A Troubling Breeze (4 quests) -> Starbreeze, back to Dolanaar, inside Barrow's Den (Oben /w Sleeping Druid in here), kill Ursal in bottom left corner.
+--   A Troubling Breeze (4 quests) -> Starbreeze, back to Dolanaar, inside Barrow's Den (Oben /w Sleeping Druid in here), back to Dolanaar, kill Ursal in bottom left corner, back to Dolanaar.
 --   Denalan's Earth: Leads to:
 --   .. Timberling Seeds (5 quests) -> seeds around denalan, delivery to darnassus, tumors in NW lake, back to darnassus (turn-in), back to denalan, kill Oakenscowl (south of lake al'ameth)
 --   .. Timberling Sprouts: Done together with the seeds.
@@ -91,5 +198,8 @@ Turn in [QT932 Twisted Hatred] and [QT2438 The Emerald Dreamcatcher].\\Accept [Q
 --   Twisted Hatred: Cave north of Dolanaar.
 --   The Glowing Fruit: After timberling sprout/seeds, can loot in SW. Turn in at Denalan.
 --   The Shimmering Frond: Can loot in NW after XXX. Turn in at Denalan. Leads to The Sprouted Fronds (instant reward).
-
+--   Crown of the Earth (6 quests) -> Fill vial, Dolanaar, Starbreeze, Dolanaar, Arlithrien (west?), Dolanaar, Glade, Dolanaar, Darnassus
+-- Extra quest starts: 937 The Enchanted Glade (3 quests) -> Kill Harpies, Go to Darnassus, Go to Aubderine 
+--    In harpy area: 938 Mist -> Just go back to NPC with cat. (10 min timer)
+--    927 The Moss-twined Heart -> Kill Blackmoss the Fetid -> Denalan
 ]], 'Shiros Speedrun')
