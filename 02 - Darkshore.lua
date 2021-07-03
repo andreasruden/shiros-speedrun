10700eada5c678b36baea8c8bdb94e2491b6c264
Andreas Ruden <andreasruden91@gmail.com>
2021-07-03 09:50:08 +0200

    Start on Darkshore

---
+++ 02 - Darkshore.lua
@@ -0,0 +1,25 @@
+Guidelime.registerGuide(
+[[
+
+[D Shiros 1-58 Speedrun for alliance (patch 2.3+)]
+[GA Alliance]
+[N 12-15 Darkshore]
+[NX 15-17 TBD]
+
+Accept [QA3524 Washed Ashore].
+Turn in [QT6342 Flight to Aubderine].\\Accept [QA6343 Return to Nessa].
+Set hearthstone in [S Auberdine].
+Accept [QA963 For Love Eternal].
+Accept [QA983 Buzzbox 827] in the room above the innkeeper.
+Accept [QA947 Cave Mushrooms].
+Accept [QA4811 The Red Crystal] when you see the wandering sentinel.
+Accept [QA2118 Plagued Lands].
+Accept [QA984 How Big a Threat?].
+Accept [QA2178 Easy Strider Living].
+Accept [QA954 Bashal'Aran].
+Accept [QA958 Tools of the Highborne].
+[OC] If you have 6 *Darkshore Grouper*: Accept [QA1141 The Family and the Fishing Pole], and turn in [QT1141 The Family and the Fishing Pole]. Note: It has no follow-up, and we're assuming you skip it!
+
+-- XXX: Getting skinning to 65+
+
+]]
