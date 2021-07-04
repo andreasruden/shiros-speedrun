4bac3c2d7cc4023aa55f15b9e3926c8e6a2199fb
Andreas Ruden <andreasruden91@gmail.com>
2021-07-04 16:09:46 +0200

    Add first part of Darkshore (up to level 14.5)

--- 02 - Darkshore.lua
+++ 02 - Darkshore.lua
@@ -6,6 +6,10 @@ Guidelime.registerGuide(
 [N 12-15 Darkshore]
 [NX 15-17 TBD]
 
+-- XXX: List which quests we have coming into this zone, for checking against.
+You should have 2 quests: Grove of the Ancients, Flight to Auberdine.
+
+-- Initial quest-state setup
 Accept [QA3524 Washed Ashore].
 Turn in [QT6342 Flight to Aubderine].\\Accept [QA6343 Return to Nessa].
 Set hearthstone in [S Auberdine].
@@ -15,11 +19,90 @@ Accept [QA947 Cave Mushrooms].
 Accept [QA4811 The Red Crystal] when you see the wandering sentinel.
 Accept [QA2118 Plagued Lands].
 Accept [QA984 How Big a Threat?].
-Accept [QA2178 Easy Strider Living].
-Accept [QA954 Bashal'Aran].
-Accept [QA958 Tools of the Highborne].
 [OC] If you have 6 *Darkshore Grouper*: Accept [QA1141 The Family and the Fishing Pole], and turn in [QT1141 The Family and the Fishing Pole]. Note: It has no follow-up, and we're assuming you skip it!
+[OC] Look out for striders to kill on the way, we will need 5 *Strider Meat* for a cooking quest.
+[OC] Get skinning to level 65+ in the next couple of steps (before coast). (At 60 you can skin up to level 12 mobs, at 65 up to 13, etc)
+Go east to the Red Crystal to complete [QC4811 The Red Crystal].
+Run south-west to the furbolg camp to complete [QC984 How Big a Threat?].
+Capture a [G38.68,58.82 Rabid Thistle Bear] around this area, using Tharnarium's Hope.
+Head north-west and loot the big, dead dinosaur to complete [QC3524 Washed Ashore].
+From here follow the coast north, killing crawlers to complete [QC983 Buzzbox 827]. (There's some north of Auberdine too)
+Turn in [QT983 Buzzbox 827].\\Accept follow-up [QA1001 Buzzbox 411].
+Turn in [QT3524 Washed Ashore].\\Accept follow-up [QA3524 Washed Ashore].
+Turn in [QT4811 The Red Crystal].\\Accept [QA4812 As Water Cascades].
+Deliver the bear at [G38.8,43.4 Tharnariun] to complete [QC2118 Plagued Lands].
+Turn in [QT2118 Plagued Lands].\\Accept [QA2138 Cleansing of the Infected].
+Turn in [QT984 How Big a Threat?].\\Accept [QA985 How Big a Threat?] and [QA4761 Thundris Windweaver].
+Accept [QA2178 Easy Strider Living].
+Turn in [QT4761 Thundris Windweaver].\\Accept [QA954 Bashal'Aran], [QA958 Tools of the Highborne] and [QA4762 The Cliffspring River].
+[OC] If you don't have a full array of bags at this point, you should buy 6 slots from [G37.48,40.48 Dalmond].
+Swim west to complete [QC3524 Washed Ashore].
+Turn in [QT3524 Washed Ashore] to unlock all the Sea Monster quests.
+If you're not level 13 yet, grind to [XP13].
+[OC] Consider [V] vendoring if you need bag space, lots of junk drops ahead. (Don't sell Strider Meat!)
+Accept [QA982 Deep Ocean, Vast Sea].
 
--- XXX: Getting skinning to 65+
+-- Making the first northern round
+Jump into the water and kill Threshers on the way north to complete [QC1001 Buzzbox 411].
+Grind towards [G41,31.7].
+Accept [QA4723 Beached Sea Creature].
+Enter the boat's [G38.24,29.40 south-western window], and swim down to your left.
+Loot [QC982,1 Silver Dawning's Lockbox].
+Enter the [G40.29,27.63 broken part] of the boat at the sea-floor, then swim left.
+Loot [QC982,2 Mist Veil's Lockbox].
+Turn in [QT1001 Buzzbox 411].\\Accept [QA1002 Buzzbox 323].
+[OC] Get skinning to 70 on the way to the next objective.
+Turn in [QT954 Bashal'Aran].\\Accept follow-up [QA955 Bashal'Aran].
+Complete [QC955 Bashal'Aran].
+Turn in [QT955 Bashal'Aran].\\Accept follow-up [QA956 Bashal'Aran].
+Complete [QC956 Bashal'Aran].
+Turn in [QT956 Bashal'Aran].\\Accept follow-up [QA957 Bashal'Aran].
+In this general area (above Bashal'aran, but below the Cliffspring river) complete the three quests [QC1002 Buzzbox 323], [QC [QC2138 Cleansing of the Infected] and [QC2178 Easy Strider Living].
+Turn in [QT1002 Buzzbox 323].\\Accept [QA1003 Buzzbox 525].
+Jump into the waterfall and fill the sampling tube to complete [QC4762 The Cliffspring River].
+Head west and accept [QA4725 Beached Sea Turtle].
+Deathwarp (die & resurrect at spirit healer) to Bashal'Aran.
+Follow the road until [G41.19,44.19].
+Then head east through the trees and jump down. [G39.97,43.88]
+Turn in [QT2138 Cleansing of the Infected].\\Accept [QA2139 Tharnariun's Hope].
+Accept [QA965 The Tower of Althalaxx].
+Turn in [QT982 Deep Ocean, Vast Sea].
+[R] Repair at Elisa, the dwarf next to the turn-in.
+Turn in [QT2178 Easy Strider Living].
+Turn in [QT4723 Beached Sea Creature].
+Turn in [QT4725 Beached Sea Turtle].
+[OC] You should be a decent way into level 14 at this point.
+Fly to [F Rut'theran Village].
+Turn in [QT6343 Return to Nessa].
+*Outside* of the Temple of Moon in Darnassus, accept [QA730 Trouble In Darkshore?].
+[T] Learn *only essential* skills at your trainer. We need to start saving for 30 mount!
+[OC] Gold check: You should have at least 1g 50s after buying skills.
+Hearth to [H Auberdine].
 
+-- Next round? Or Loch Modan?
+
+
+-- XXX: Once Level 15:
+Turn in [QT4762 The Cliffspring River].\\Accept [QA4763 The Blackwood Corrupted].
+Fill tube for this + red crystal follow up in auberdine moonwell.
+
+-- Quest Chains of Darkshore:
+-- Buzzbox -> kill crawlers -> Auberdine -> kill Thresher -> NW coast -> Moonstalker Fang (together with rabid bears in north) -> Cliffspring River waterfall -> Grizzled Scalp in south (around lost master / expedition) -> Grove of the Ancients
+-- Bashal'Aran -> Bashal'Aran -> Kill Grells -> Kill Satyrs -> destroy seal @ Ameth'Aran -> Bashal'Aran
+-- Plagued Lands -> Capture a Rabid Thistle Bear (en masse north of Bashal or south of Ameth, but there exists a couple just south of the furbolgs for How big a threat?) -> Auberdine -> Kill 20 (north with moonstalker) -> Auberdine -> Kill Den Mother in NE cave (below river) -> Auberdine
+-- Washed Ashore -> Coast just below Auberdine -> Auberdine -> Out in water west of Auberdine -> Auberdine -> Unlocks all Beached Creatures
+-- The Red Crystal -> Crystal east of Auberdine -> Auberdine (fill tube) -> Back to same Crystal -> Auberdine
+-- How Big a Threat -> Scout camp south of Auberdine -> Auberdine (unlocks Thundris Windweaver) -> Kill @ South Camp -> Auberdine (unlocks A Lost Master?)
+-- Thundris Windweaver -> Auberdine -> Waterfall @ Cliffspring River bridge -> Auberdine (Fill @ moonwell) -> Do cleaning ritual close to Cliffspring River cave
+-- A Lost Master: Moonstalker Sires/Matriarches Very South (pretty bad quest) -> Auberdine -> Cave in south-east -> Escort Volcor -> Auberdine -> Breadcrumb to Astranaar
+-- Cave Mushrooms -> NE cave (above river) -> Auberdine -> Grove of Ancients -> Very South -> Still in Twilight Camp -> Grove of Ancients -> Relics at Tower of Althalaxx
+-- Tower of Althalaxx (req 13) -> Tower of Althalaxx -> Kill fanatics for 4 Worn Parchments -> Ashenvale (Maestra's Post) -> 3 quests in Ashenvale (incl far east) -> back to Tower of Althalaxx -> Get Head of Athrikus (really nasty quest) -> Back to Post
+-- Gyromast's Retrieval (req 13) -> Accept very north, all done in this area too -> rewards 5 Elixir of Wather Breathing!
+-- The Absent Minded Prospector -> Accept (Trouble In Darkshore?, req 14) in Darnassus -> Auberdine -> Go South & do really nasty escort -> Auberdine -> Darnassus -> Menethil Harbor
+
+-- One-off quests:
+-- Tools of the Highborne: Ameth'aran
+-- For Love Eternal: Ameth'aran
+-- The Fall of Ameth'Aran: picked up by & done by Ameth'aran
+-- Note: There are 9 beached sea monsters after you completed Washed Ashore pt 1 and 2. Three north of Auberdine, and six south.
 ]]
