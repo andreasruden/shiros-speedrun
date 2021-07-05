e8128ab98cdd758f86fb1af78a7afc50db400302
Andreas Ruden <andreasruden91@gmail.com>
2021-07-05 22:07:12 +0200

    Start on Loch Modan

--- 03 - Loch Modan.lua
+++ 03 - Loch Modan.lua
@@ -6,4 +6,48 @@ Guidelime.registerGuide(
 [N 15-17 Loch Modan]
 [NX 17-19 Darkshore]
 
+-- Arriving in Thelsamar
+Accept [QA224 In Defense of King's Lands].
+Inside the bunker, accept [QA267 The Trogg Threat].
+In [G30.79,70.22 Stonesplinter Valley], complete [QC224 In Defense of King's Lands].
+Finish up [QC267 The Trogg Threat].
+Turn in [QT224 In Defense of King's Lands].
+Accept [QA237 In Defense of King's Lands].
+Turn in [QT267 The Trogg Threat].
+Complete [QC237 In Defense of King's Lands] in [G28.9,83.9 Stonesplinter Valley].
+Turn in [QC237 In Defense of King's Lands].
+Accept follow-up [QA263 In Defense of King's Lands].
+Get fligh point in [P Thelsamar].
+[OC] If you see the patrol, accept his quests.
+[V][R] Vendor & repair at [G34.08,46.69 Morhan].
+Accept [QA255 Mercenaries].
+In the inn, accept [QA418 Thelsamar Blood Sausages].
+Accept [QA256 WANTED: Chok'sul].
+In the house behind the wanted poster, accept [QA436 Ironband's Excavation].
+Accept [QA416 Rat Catching] and [QA1339 Mountaineer Stormpike's Task].
+
+-- Finish the low level quests in the north-west
+[OC] On the way to the next objectives, kill mobs for [QC418 Thelsamar Blood Sausages].
+[G34.74,30.67]
+Turn in [QT1339 Mountaineer Stormpike's Task].
+Accept [QA1338 Stormpike's Order].
+Accept [QA307 Filthy Paws].
+Complete [QC307 Filthy Paws] in the [G35.4,18.5 Silver Stream Mine].
+Finish up [QC416 Rat Catching].
+Turn in [QT307 Filthy Paws].
+On the way back to Thelsamar, complete [QC418 Thelsamar Blood Sausages].
+Turn in [QT416 Rat Catching].
+Turn in [QT418 Thelsamar Blood Sausages].
+[V] Vendor at the dwarf opposite the sausage turn-in.
+
+-- Making the big Loch Modan round
+
+-- Loch Modan Chains:
+-- In Defense of the King's Lands: Troggs+Scouts in Stonesplinter.V. -> Back to bunker -> Thumpers+Seers in valley or northern island* -> Back to bunker -> Shaman+Bonesnapper in S.V. cave or northern island* -> Back to bunker -> Named mobs in cave
+-- Stormpike -> Mountaineer's Task (northern bunker) -> Orders to stormwind. Note: Nearby in SW you can accept Stormpike's Delivery which is back to Loch Modan
+-- A hunter's boast: Kill Buzzard in SE -> back to lodge -> kill bears in east (middle of map) -> back to lodge -> Unlocks Vyrin's Revenge (kill Ol' Sooty in cave south of Thelsamar) -> back to lodge
+-- Excavation Progress Report -> Starts in excavation site -> Ironforge -> Thelsamar -> Huldar -> Escort Miran to Site
+
+-- *This northern island is where follow up to Find Bingles takes place
+
 ]], 'Shiros Speedrun')
