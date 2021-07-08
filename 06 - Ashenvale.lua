877d771a8831250db20904c88b2794b856d3f0a4
Andreas Ruden <andreasruden91@gmail.com>
2021-07-08 21:38:01 +0200

    Ashenvale started

---
+++ 06 - Ashenvale.lua
@@ -0,0 +1,44 @@
+Guidelime.registerGuide(
+[[
+
+[D Shiros 1-58 Speedrun for alliance (patch 2.3+)]
+[GA Alliance]
+[N 22-25 Ashenvale]
+[NX X-X XXX]
+
+-- XXX: Maybe we should do the escort and stuff into Ashenvale now instead of earlier? The benefit would be not having to backtrack into Darkshore. We'd also do Maestra's post on the way. But, we still have to go to astranaar to pickup Zoram Strand and Raene's Cleansing before finishing west. So in the end it should not have major difference.
+
+-- Setup
+Turn in [QT3765 The Corruption Abroad].
+Fly to [F Astranaar].
+Accept [QA1008 The Zoram Strand].
+-- A Helping Hand
+-- On Guard in Stonetalon
+-- Journey to Stonetalon Peak
+Accept [QA991 Raene's Cleansing] and [QA1054 Culling the Threat].
+Set hearthstone in [S Astranaar].
+
+-- Western Ashenvale + Zoram Strand
+Follow the road west, then on the path leading north run up and complete [QC1054 Culling the Threat].
+[G37.48,32.3]
+Jump down from the tree [G38,30]
+[OC] Continue climbing down, then head to Maestra's Post.
+Turn in [QT967 The Tower of Althalaxx].\\Accept follow-up [QA970 The Tower of Althalaxx].
+Accept [QA1010 Bathran's Hair].
+Complete [QC970 The Tower of Althalaxx] (pretty bad drop rate).
+Complete [QC1010 Bathran's Hair].
+Turn in [QT970 The Tower of Althalaxx].\\Accept follow-up [QA973 The Tower of Althalaxx].
+Turn in [QT1010 Bathran's Hair].\\Accept [QA1020 Orendil's Cure].
+
+-- Stonetalon??
+Turn in [QT1020 Orendil's Cure].\\Accept [QA1033 Elune's Tear].
+[G41.3,50.97]
+[G42.5,48.0]
+[G43.2,45.8]
+[G44.68,45.46]
+Complete [QC1033 Elune's Tear].
+Turn in [QT1033 Elune's Tear].\\Accept [QA1034 The Ruins of Stardust].
+
+-- Head up and finish Darkshore
+
+]], 'Shiros Speedrun')
