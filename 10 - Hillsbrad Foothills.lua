39cb92065dca5bd204d9b46f75fde24c78d4c27d
Andreas Ruden <andreasruden91@gmail.com>
2021-07-12 20:42:03 +0200

    Finish up Hillsbrad Foothills pt 1

--- 10 - Hillsbrad Foothills.lua
+++ 10 - Hillsbrad Foothills.lua
@@ -9,16 +9,15 @@ Guidelime.registerGuide(
 [OC] As of patch 2.3+, quests Lv.30 or higher are rewarding increased xp.
 
 -- Southshore
--- Bartolo's Yeti Fur Cloak
--- Crushridge Bounty
--- Syndicate Assassins
--- Hints of a New Plague?
--- Missing Crystals
+-- Bartolo's Yeti Fur Cloak (Shit quest, skip)
+-- Crushridge Bounty (XXX: do later at like 36-37, together with Noble Deaths? or do we squeeze it in here to have Crushridge Warmongers available?)
+Accept [QA505 Syndicate Assassins].
+Accept [QA569 Hints of a New Plague?]
+Accept [QA9435 Missing Crystals].
 Set hearthstone in [S Southshore].
 Accept [QA536 Down the Coast].
--- From the chef, Soothing Turtle Bisque
--- Letter to Stormpike
--- Costly Menace
+From the chef, [QA555 Soothing Turtle Bisque].
+Accept [QA564 Costly Menace].
 Run southwest of Southshore and complete [QC536 Down the Coast].
 Turn in [QT536 Down the Coast].\\Accept [QA559 Farren's Proof].
 At the same beach, complete [QC559 Farren's Proof].
@@ -26,11 +25,30 @@ Turn in [QT559 Farren's Proof].\\Accept follow-up [QA560 Farren's Proof].
 Turn in [QT560 Farren's Proof].\\Accept follow-up [QA561 Farren's Proof].
 Turn in [QT561 Farren's Proof].\\Accept [QA562 Stormwind Ho!].
 On the beach SE of southshore, complete [QC562 Stormwind Ho!].
+
+-- Making the rounds
+After finishing, head to the [G55.4,58.5 river].
+[OC] Kill turtles for [QC555 Soothing Turtle Bisque] progress.
+Grind over to [G60.1267,43.8959,2 here], where you can climb up the left side of the waterfall by hugging the water + rock.
+Kill a turtle or two right after [G63.27,40.67 climbing] the waterfall.
+Then head over and complete [QC9435 Missing Crystals] inside the tower.
+Go back to the river and keep grinding turtles north until you complete [QC555 Soothing Turtle Bisque].
+From the river, go to [G67.19,8.96 in Hillsbrad Foothills] (66,69 if zone is Alterac Mountains).
+From here run up to Sofera's Naze and from the document on the table accept [QA510 Foreboding Plans] and [QA511 Encrypted Letter].
+Finish up [QC505 Syndicate Assassins] [G58.3,67.9].
+Continue heading west to complete [QC564 Costly Menace].
+Run back to southshore to turn in [QT564 Costly Menace].
+Turn in [QT511 Encrypted Letter].\\Accept [QA514 Letter to Stormpike].
+Purchase [CI3713,1 Soothing Spices] from [G48.93,55.03 Micha].
+Turn in [QT9435 Missing Crystals].
 Turn in [QT562 Stormwind Ho!].\\Accept [QA563 Reassignment].
-If necessary, grind the beaches until [XP32-11850].
+At the chef, turn in [QT555 Soothing Turtle Bisque].
+Turn in [QT505 Syndicate Assassins] and [QT510 Foreboding Plans].\\Accept [QA512 Noble Deaths].
+You should be well into Lv.32 by now, but if not grind the beaches until [XP32-11850].
 
 -- IF
 Fly to [F Ironforge].
+[OC] Possible to turn in donation of wool/silk here.
 Learn new bandages for FA, if you can [G54.92,58.43] (115 for heavy wool, 150 for silk).
 Turn in [QT637 Sully Balloo's Letter].\\Accept [QA683 Sara Balloo's Plea].
 Turn in [QT683 Sara Balloo's Plea].\\Accept [QA686 A King's Tribute].
@@ -43,10 +61,11 @@ Turn in [QT563 Reassignment].
 [OC] Passing by The Perenolde Tiara quest XXX: Do we wanna do this? Probably not.
 Turn in [QT293 Cleansing the Eye].
 Accept [QA690 Malin's Request].
+Accept [QA1301 James Hyal].
 Fly to [F Sentinel Hill].
 Turn in [QT325 Armed and Ready].\\Accept [QA55 Morbent Fel].
 Equip the off-hand early to let 30 sec CD reset. Use on Morbent and kill him to complete [QC55 Morbent Fel].
 Turn in [QT55 Morbent Fel].
-Hearth to [H Southshore].
+Run back to Sentinel Hill and fly to [F Rebel Camp]. -- XXX: Quicker than just running straight to rebel camp?
 
 ]], 'Shiros Speedrun')
