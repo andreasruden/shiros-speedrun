127851c68c39578d2ceb4f4f3e4c3b4753626af1
Andreas Ruden <andreasruden91@gmail.com>
2021-07-15 11:12:01 +0200

    Start on Dustwallow

--- 15 - Dustwallow Marsh.lua
+++ 15 - Dustwallow Marsh.lua
@@ -11,7 +11,7 @@ Accept [QA11191 This Old Lighthouse].
 Turn in [QT11191 This Old Lighthouse].\\Accept [QA11192 Thresher Oil].
 Accept [QA11126 Traitors Among Us].
 [OC] As you're running around, complete [QC11126 Traitors Among Us].
-Accept [QA1282 They Call Him Smiling Jim] and [QA11212 Tabetha's Farm].
+Accept [QA1282 They Call Him Smiling Jim]. -- XXX: Sounds like you cannot actually get 1282 if you've completed James Hyal (which gives more xp)
 Set hearthstone in [S Theramore].
 Turn in [QT1260 Morgan Stern].\\Accept [QA1204 Mudrock Soup and Bugs].
 Turn in [QT1282 They Call Him Smiling Jim].\\Accept [QA11124 Inspecting the Ruins].
@@ -34,6 +34,39 @@ Accept [QA11136 A Disturbing Development].
 Turn in [QT11136 A Disturbing Development].\\Accept [QA11137 Defias in Dustwallow?]
 Turn in [QT11177 The Hermit of Swamplight Manor].\\Accept [QA1218 Marsh Frog Legs].
 Accept [QA11180 What's Haunting Witch Hill?]
+Complete [QC1218 Marsh Frog Legs] and [QC11180 What's Haunting Witch Hill?] around the hut.
+Turn in [QT1218 Marsh Frog Legs].\\Accept [QA1206 Jarl Needs Eyes].
+Turn in [QC11180 What's Haunting Witch Hill?].\\Accept [QA11181 The Witch's Bane].
 Accept [QA1219 The Orc Report].
+[OC] Go through marsh rather than road. Loot Witchbanes during next two steps.
+Accept [QA11146 Raptor Captor].
+Turn in [QT1266 The Missing Diplomat].\\Accept follow-up [QA1324 The Missing Diplomat].
+Beat up the private to complete [QC1324 The Missing Diplomat].
+Complete [QC11146 Raptor Captor]. [G47,18]
+Accept escort [QA1222 Stinky's Escape]. Grind raptors if you have to wait.
+Complete [QC1222 Stinky's Escape].
+Turn in [QT11146 Raptor Captor].\\Accept [QA11145 Prisoners of the Grimtotems] and [QA11147 Unleash the Raptors]. -- Done together with A Grim Connection line
+In the spider cavern to the north-west complete [QC1206 Jarl Needs Eyes].
+Accept [QA1177 Hungry!]
+Turn in [QT11123 Inspecting the Ruins].
+Accept [QA1284 Suspicious Hoofprints].
+Accept [QA1252 Lieutenant Paval Reethe].
+Accept [QA1253 The Black Shield].
+Hearth to [H Theramore].
+
+-- Theramore
+Accept [QA11212 Tabetha's Farm].
+
+-- XXX: Sometime
+
+Turn in [QT11212 Tabetha's Farm]. -- XXX: Help for Mudsprocket req Lv.38
+
+-- QuestNotes
+-- Discrediting the Deserters -> All around Theramore
+-- This Old Lighthouse: Around Theramore -> To Nat Page -> Tethyr fight
+-- They Call Him Smiling Jim -> Starts Theramore -> Loot 3 items at shady rest inn -> Theramore turn-in
+  -- One of the items (Lieutenant Paval Reethe) goes outside of theramore -> Theramore -> SE of inn -> Theramore
+  -- Once all 3 are done. Unlocks:
+    -- A Grim Connection (starts in Theramore) -> North Central Tower (starts Raptor Captor, ends Missing Diplomat) -> Northern Grimtotem Village (other one has no Elders) -> Armaments in same village (same time as elders?) -> Speak with Tabetha -> Burn southern Grimtotem Village -> Theramore
 
 ]], 'Shiros Speedrun')
