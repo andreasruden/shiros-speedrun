a3e120277d5296f0fbabaea4d0e44fa19991cd68
Andreas Ruden <andreasruden91@gmail.com>
2021-07-15 18:18:32 +0200

    Complete Dustwallow Marsh

--- 15 - Dustwallow Marsh.lua
+++ 15 - Dustwallow Marsh.lua
@@ -3,15 +3,17 @@ Guidelime.registerGuide(
 
 [D Shiros 1-58 Speedrun for alliance (patch 2.3+)]
 [GA Alliance]
-[N 37-39 Dustwallow Marsh]
+[N 37-40 Dustwallow Marsh]
 [NX X-X X]
+
+-- XXX: First Aid
 
 -- Theramore
 Accept [QA11191 This Old Lighthouse].
 Turn in [QT11191 This Old Lighthouse].\\Accept [QA11192 Thresher Oil].
 Accept [QA11126 Traitors Among Us].
 [OC] As you're running around, complete [QC11126 Traitors Among Us].
-Accept [QA1282 They Call Him Smiling Jim]. -- XXX: Sounds like you cannot actually get 1282 if you've completed James Hyal (which gives more xp)
+?? Accept [QA1282 They Call Him Smiling Jim]. -- XXX: Sounds like you cannot actually get 1282 if you've completed James Hyal (which gives more xp)
 Set hearthstone in [S Theramore].
 Turn in [QT1260 Morgan Stern].\\Accept [QA1204 Mudrock Soup and Bugs].
 Turn in [QT1282 They Call Him Smiling Jim].\\Accept [QA11124 Inspecting the Ruins].
@@ -45,7 +47,7 @@ Beat up the private to complete [QC1324 The Missing Diplomat].
 Complete [QC11146 Raptor Captor]. [G47,18]
 Accept escort [QA1222 Stinky's Escape]. Grind raptors if you have to wait.
 Complete [QC1222 Stinky's Escape].
-Turn in [QT11146 Raptor Captor].\\Accept [QA11145 Prisoners of the Grimtotems] and [QA11147 Unleash the Raptors]. -- Done together with A Grim Connection line
+Turn in [QT11146 Raptor Captor]. Skip follow-ups for now.
 In the spider cavern to the north-west complete [QC1206 Jarl Needs Eyes].
 Accept [QA1177 Hungry!]
 Turn in [QT11123 Inspecting the Ruins].
@@ -55,11 +57,97 @@ Accept [QA1253 The Black Shield].
 Hearth to [H Theramore].
 
 -- Theramore
+Turn in [QT1222 Stinky's Escape].
 Accept [QA11212 Tabetha's Farm].
+Turn in [QT1284 Suspicious Hoofprints], [QT1252 Lieutenant Paval Reethe] and [QT1253 The Black Shield].\\Accept follow-ups [QA1259 Lieutenant Paval Reethe] and [QA1319 The Black Shield].
+Turn in [QT1259 Lieutenant Paval Reethe].\\Accept [QA1285 Daelin's Men].
+Turn in [QT1285 Daelin's Men].\\Accept [QA1286 The Deserters].
+Turn in [QT1219 The Orc Report].\\Accept [QA1220 Captain Vimes]. -- XXX: Why can't I accept captain vimes on the priv server?
+Turn in [QT1324 The Missing Diplomat].\\Accept follow-up [QA1267 The Missing Diplomat].
+Turn in [QT1267 The Missing Diplomat].\\Accept follow-up [QA1267 The Missing Diplomat]. -- XXX: Seems you can turn this in after subduing the guy in the previous part? Jaina spawns or something.
+Turn in [QT1319 The Black Shield].\\Accept follow-up [QA1320 The Black Shield].
+Turn in [QT1220 Captain Vimes] and [QT1320 The Black Shield].
 
--- XXX: Sometime
+-- Mudsprocket
+Fly to [F Mudsprocket].
+Accept [QA11217 Catch a Dragon by the Tail].
+Accept [QA11184 WANTED: Goreclaw the Ravenous].
+Accept [QA11158 Bloodfen Feathers] and [QA11159 Spirits of Stonemaul Hold].
+Complete [QC11184 WANTED: Goreclaw the Ravenous].
+Kill raptors to finish up [QC11158 Bloodfen Feathers]. [G32,66]
+Turn in [QT1286 The Deserters].\\Accept follow-up [QA1287 The Deserters].
+[OC] Help for Mudsprocket req Lv.38, which you should be. If not pick it up next time we're by.
+Turn in [QT11212 Tabetha's Farm].\\Accept [QA11211 Help for Mudsprocket].
+Accept [QA11169 The Grimtotem Weapon] and [QA11173 The Reagent Thief].
+-- Accept [QA11156 Direhorn Raiders]. XXX: Can accept this later together with the inn line
+[G46,62 Circle around Tabetha's Farm] to complete [QC11173 The Reagent Thief] and [QC11169 The Grimtotem Weapon].
+Turn in [QT11169 The Grimtotem Weapon] and [QT11173 The Reagent Thief].
+Accept [QA11172 The Zeppelin Crash].
+Turn in [QT11172 The Zeppelin Crash].\\Accept [QA11207 Secure the Cargo!] and [QA11174 Corrosion Prevention].
+[OC] Around this area there's power fragments that energize you, when energized use the item to dissolve a slime.
+Go around looting boxes and dissolving slimes to complete [QC11207 Secure the Cargo!] and [QC11174 Corrosion Prevention].
+Turn in [QT11207 Secure the Cargo!] and [QT11174 Corrosion Prevention].\\Accept [QA11208 Delivery for Drazzit].
+Turn in [QT11194 Is it Real?]\\Accept [QA11209 Nat's Bargain].
+Apply the paste to yourself, then enter the water and swim to the ship. Kill the shark that attacks you to complete [QC11209 Nat's Bargain].
+Turn in [QT11209 Nat's Bargain].\\Accept [QA11210 Oh, It's Real].
 
-Turn in [QT11212 Tabetha's Farm]. -- XXX: Help for Mudsprocket req Lv.38
+-- Theramore
+Swim to Theramore or hearthstone and turn in [QT11210 Oh, It's Real].\\Accept [QA11198 Take Down Tethyr!]
+Complete [QC11198 Take Down Tethyr!] by using the cannons.
+Turn in [QT11198 Take Down Tethyr!]
+Turn in [QT1287 The Deserters].\\Accept [QA11143 A Grim Connection].
+-- XXX: Mission to Mudsprocket is exclusive to the one from Tabetha on priv servers, is this true for retail too?
+
+-- Full Round
+Kill Garn Mathers and complete [QC11137 Defias in Dustwallow?]
+Turn in [QT11137 Defias in Dustwallow?]\\Accept [QA11138 Renn McGill].
+Turn in [QT1206 Jarl Needs Eyes].\\Accept [QA1203 Jarl Needs a Blade].
+[OC] Collect Witchbanes on the way.
+Kill Razorspine and loot him to complete [QC1203 Jarl Needs a Blade].
+[OC] Kill any turtle you spot.
+Turn in [QT11138 Renn McGill].\\Accept [QA11139 Secondhand Diving Gear].
+Complete [QC11139 Secondhand Diving Gear].\\Accept [QA11140 Recover the Cargo!]
+Open the Supplies, put on the helmet and complete [QC11140 Recover the Cargo!]
+Turn in [QT11140 Recover the Cargo!]\\Accept [QA11141 Jaina Must Know].
+Finish up [QC1204 Mudrock Soup and Bugs] and [QC1177 Hungry!] [G62,8]
+Finish up [QC11181 The Witch's Bane].
+Turn in [QT11181 The Witch's Bane].\\Accept [QA11183 Cleansing Witch Hill].
+Use the torch at the end of the dock and kill the spawn to complete [QC11183 Cleansing Witch Hill].
+Turn in [QT11183 Cleansing Witch Hill].
+Turn in [QT1203 Jarl Needs a Blade].
+Turn in [QT11143 A Grim Connection].\\Accept [QA11144 Confirming the Suspicion] and [QA11148 Arms of the Grimtotems].
+Accept [QA11145 Prisoners of the Grimtotems] and [QA11147 Unleash the Raptors].
+[OC] Elders drop fragments for battle plans. Keys drop from all mobs.
+Complete [QC11147 Unleash the Raptors], [QC11144 Confirming the Suspicion], [QA11148 Arms of the Grimtotems] and [QC11145 Prisoners of the Grimtotems].
+Turn in [QT11145 Prisoners of the Grimtotems] and [QT11147 Unleash the Raptors].
+Turn in [QT11144 Confirming the Suspicion] and [QT11148 Arms of the Grimtotems].\\Accept [QA11149 Tabetha's Assistance].
+Turn in [QT1177 Hungry!]
+Grind your way over to Tabetha and turn in [QT11149 Tabetha's Assistance].\\Accept [QA11150 Raze Direhorn Post!]
+Accept [QA11156 Direhorn Raiders].
+Complete [QC11150 Raze Direhorn Post!] and [QC11156 Direhorn Raiders].
+Turn in [QT11156 Direhorn Raiders].
+Turn in [QC11150 Raze Direhorn Post!]\\Accept [QA11151 Justice for the Hyals].
+
+-- Finish up at Mudsprocket
+Complete [QC11159 Spirits of Stonemaul Hold].
+Turn in [QT11211 Help for Mudsprocket], [QT11184 WANTED: Goreclaw the Ravenous] and [QT11208 Delivery for Drazzit].
+Turn in [QT11158 Bloodfen Feathers] and [QT11159 Spirits of Stonemaul Hold].\\Accept [QA11160 Banner of the Stonemaul], [QA11161 The Essence of Enmity] and [QA11162 Challenge to the Black Flight].
+Complete [QC11160 Banner of the Stonemaul] inside the cave (keep to the left) and [QC11161 The Essence of Enmity] (use the totem on a corpse).
+On the way to Onyxia's Lair, complete [QC11217 Catch a Dragon by the Tail]. [G46.7,77.2]
+Complete [QC11162 Challenge to the Black Flight].
+Turn in [QT11217 Catch a Dragon by the Tail].
+Turn in [QT11160 Banner of the Stonemaul], [QT11161 The Essence of Enmity] and [QC11162 Challenge to the Black Flight].
+
+-- Theramore finish up
+Hearh to [H Theramore] or fly there.
+Turn in [QT1204 Mudrock Soup and Bugs].\\Accept [QA1258 ... and Bugs].
+Turn in [QT11141 Jaina Must Know].\\Accept [QA11142 Survey Alcaz Island]. -- XXX: Could save one run up the tower by turning in the diplomat chain here too.
+Turn in [QT11151 Justice for the Hyals].\\Accept [QA11152 Peace at Last].
+Use the Wreath to complete [QC11152 Peace at Last]. [G63.7,42.9]
+Complete [QC11142 Survey Alcaz Island]. [G67.3,51.2]
+Turn in [QT11142 Survey Alcaz Island].\\Accept [QA11222 Warn Bolvar!]
+Turn in [QT11152 Peace at Last].
+Take the boat from the middle dock to Menethil Harbor.
 
 -- QuestNotes
 -- Discrediting the Deserters -> All around Theramore
