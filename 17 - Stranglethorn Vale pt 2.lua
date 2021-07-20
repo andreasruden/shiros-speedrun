7eda41780c4300576aa68767409020f0054dea8e
Andreas Ruden <andreasruden91@gmail.com>
2021-07-20 19:25:25 +0200

    Work on STV pt2

--- 17 - Stranglethorn Vale pt 2.lua
+++ 17 - Stranglethorn Vale pt 2.lua
@@ -6,25 +6,63 @@ Guidelime.registerGuide(
 [N 42-43 Stranglethorn Vale]
 [NX X-X X]
 
+XXX: We need [QA205 Troll Witchery] [QA202 Colonel Kurzen] (rebel camp) [QA192 Panther Mastery] [QA196 Raptor Mastery] (nessingwary) from last time.
+
 -- Booty Bay
 -- Tran'rek
--- Skullsplitter Tusks
+Accept [QA209 Skullsplitter Tusks].
 Turn in [QT669 Sunken Treasure]. -- \\Accept [QA670 Sunken Treasure].
 Run down the stairs and turn in [QT603 Ansirem's Key].\\Accept [QA610 "Pretty Boy" Duncan].
--- Whiskey Slim's Lost Grog
--- Zanzil's Secret
--- Akiris by the Bundle
--- Stranglethorn Fever
--- Scaring Shaky and Stoley's Debt
--- The Bloodsail Buccaneers
--- The Captain's Chest
+Accept [QA580 Whiskey Slim's Lost Grog].
+Accept [QA621 Zanzil's Secret].
+Accept [QA617 Akiris by the Bundle].
+Accept [QA348 Stranglethorn Fever].
+Accept [QA606 Scaring Shaky] and [QA2872 Stoley's Debt].
+Accept [QA595 The Bloodsail Buccaneers].
+Accept [QA614 The Captain's Chest].
 
--- Rebel Camp
-Accept [QA205 Troll Witchery].
-Accept [QA202 Colonel Kurzen].
--- Nessingwary
-Accept [QA192 Panther Mastery].
-Accept [QA196 Raptor Mastery].
+-- Southern STV
+-- Just outside BB
+Kill & loot Duncan to complete [QA610 "Pretty Boy" Duncan].
+Turn in [QT595 The Bloodsail Buccaneers].\\Accept follow-up [QA597 The Bloodsail Buccaneers].
+-- XXX: There is no way in which it would be time efficient to get https://tbc.wowhead.com/quest=48/sweet-amber from westfall, right?
+Head north along the coast and complete [QC617 Akiris by the Bundle].
+Head out across the road to get to the Gorilla place. [G28.7,58.2]
+[OC] Loot a Gorilla Fang during next step.
+Head a bit further north and complete [QC606 Scaring Shaky].
+Start the event at Ubagwa and kill the waves of enemies to complete [QC348 Stranglethorn Fever].
+-- Booty Bay
+Run back to Booy Bay and turn in [QT606 Scaring Shaky].\\Accept [QA607 Return to MacKinley].
+Turn in [QT597 The Bloodsail Buccaneers].\\Accept follow-up [QA599 The Bloodsail Buccaneers].
+Turn in [QT348 Stranglethorn Fever].
+Turn in [QT607 Return to MacKinley].\\Accept [QA609 Voodoo Dues].
+Turn in [QT617 Akiris by the Bundle]. --\\Accept follow-up [QA623 Akiris by the Bundle].
+Walk into the inn and set hearthstone in [S Booty Bay].
+Walk up the stairs of the inn, turn in [QT610 "Pretty Boy" Duncan].\\Accept [QA611 The Curse of the Tides].
+Continue up the in. Accept [QA587 Up to Snuff].
+Turn in [QT599 The Bloodsail Buccaneers].\\Accept follow-up [QA604 The Bloodsail Buccaneers].
+On the way out of BB run the upper route and accept [QA576 Keep An Eye Out] from Dizzy.
+
+-- The full run from south to north
+[OC] Loot the bottles on the beach as you enter the area, to give them time to respawn.
+Go to the beach east of BB and complete [QC604 The Bloodsail Buccaneers].
+Loot bottles on the to obtain [CI4098,1 Carefully Folded Note]. Do not accept the quest yet. If you did not find the item, skip this step for now. There's a second chance.
+Complete [QC614 The Captain's Chest].
+Obtain [QC609,3 Chucky's Huge Ring]. Also clear the entire camp for Mixture progress.
+Obtain [QC609,2 Jon-Jon's Golden Spyglass].
+Obtain [QC609,1 Maury's Clubbed Foot].
+Finish up [QC621 Zanzil's Secret].
+Go south of Gurubashi Arena. [G30.1,54.1]
+Once you've continued circling around, kill Tethis and complete [QC197 Raptor Mastery].
+Head east to the goblins and complete [QC600 Venture Company Mining].
+-- XXX: Is it worth to get and do https://tbc.wowhead.com/quest=735/the-star-the-hand-and-the-heart ? Starts in Badlands, goes here + alterac + dustwallow
+Continue north while following the mountain. [G43,38.5]
+Complete [QC209 Skullsplitter Tusks] and [QC205 Troll Witchery] either in Ziata'jai (small camp) or Zul'Mamwe (big). [G45.5,42.9]
+Complete [QC628 Excelsior] along the coast.
+Complete [QC611 The Curse of the Tides]. This quest is weird, on retail the goblin was just patrolling the island without using the Altar at (24,23) in the water???
+Hearth to [H Booty Bay].
+
+-- Booty Bay
 
 -- Going to the middle
 Complete [QC196 Raptor Mastery] and [QC192 Panther Mastery] on the coast south of Grom'Gol.
