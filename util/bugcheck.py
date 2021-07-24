7803117330f8cabc2b82ccbaf4beacb0182e266d
Andreas Ruden <andreasruden91@gmail.com>
2021-07-24 20:55:30 +0200

    Add a util tool for bugfixing

--- util/bugcheck.py
+++ util/bugcheck.py
@@ -1,38 +1,115 @@
 import glob
 import sys
 
+warned_quests = []
+
+def error(line, msg):
+    print('  ERROR at line %d: %s.' % (line, msg))
+    sys.exit(-1)
+
+def warning(line, questid, msg):
+    if questid in warned_quests:
+        return
+    print('  Warning for quest %s at line %d: %s.' % (questid, line, msg))
+    warned_quests.append(questid)
+
+def print_questlog(route):
+    questlog = route.accepted + route.completed
+
+    # FIXME: Add sorting + categories that correspond to sorting it would have in-game
+
+    for quest in questlog:
+        print('%s (%s)' % ('', quest), end=', ')
+    print()
+
+def is_preceeded_by(line, n, text):
+    numwords = len(text.split())
+    
+    while True:
+        ws = 0
+        s = ''
+        i = line.find(' [Q', n-2)
+        while line[i] == ' ':
+            i -= 1
+        while ws < numwords and i >=0 and line[i] != ']':
+            s = line[i] + s
+            if line[i] == ' ':
+                ws += 1
+            i -= 1
+        if i < 0:
+            ws += 1
+        if ws == numwords and s.strip().lower().rfind(text.lower()) > -1:
+            return True
+        elif ws == 1 and s.strip() == ',' or s.strip().lower() == 'and':
+            n = line.rfind(' [Q', 0, n)
+            if n == -1:
+                return False
+        else:
+            return False
+
 def process_line(line, route, linenum):
     n = 0
     while True:
-        try:
-            (quest, n, instr) = find_next_quest_with_instruction(line, n)
-        except:
-            print('ERROR: Invalid quest formatting at line %d.' % linenum)
-            sys.exit(-1)
+        #try:
+        (quest, n, start) = find_next_quest_with_start(line, n)
+        #except:
+            #error(linenum, 'Invalid quest formatting')
         if quest == None:
             return
-        # print(instr, quest)
+        if quest.op not in ['A', 'C', 'T']:
+            error(linenum, 'Invalid operation for quest `Q%s`' % quest.op)
+        
+        if is_preceeded_by(line, start, 'Accept') and quest.op != 'A':
+            warning(linenum, quest.id, 'Opcode is `%s` but instruction says `Accept`' % quest.op)
+        elif is_preceeded_by(line, start, 'Complete') and quest.op != 'C':
+            warning(linenum, quest.id, 'Opcode is `%s` but instruction says `Complete`' % quest.op)
+        elif is_preceeded_by(line, start, 'Turn in') and quest.op != 'T':
+            warning(linenum, quest.id, 'Opcode is `%s` but instruction says `Turn in`' % quest.op)
+    
+        if quest.op == 'A' and quest.id in route.accepted:
+            warning(linenum, quest.id, 'Has already been accepted')
+        elif quest.op != 'T' and quest.id in route.completed:
+            warning(linenum, quest.id, 'Has already been completed')
+        elif quest.id in route.finished:
+            warning(linenum, quest.id, 'Has already been turned in')
 
-def find_next_quest_with_instruction(line, n=0):
+        if quest.op == 'A':
+            route.accepted.append(quest.id)
+        elif quest.op == 'C' and ',' not in quest.id:
+            if quest.id not in route.accepted:
+                error(linenum, 'Cannot complete quest %s that has not been accepted' % quest.id)
+            route.accepted.remove(quest.id)
+            route.completed.append(quest.id)
+        elif quest.op == 'T':
+            if quest.id in route.accepted:
+                route.accepted.remove(quest.id)
+            if quest.id in route.completed:
+                route.completed.remove(quest.id)
+            route.finished.append(quest.id)
+        
+        # FIXME: Check if the name is correct
+
+def find_next_quest_with_start(line, n=0):
     instr = ''
     space = False
     while n < len(line):
         char = line[n]
         if char == '-' and line[n+1] == '-':
-            return (None, len(line), 'None')
+            return (None, len(line), -1)
         elif char == ' ':
             space = True
             n += 1
         elif char == '[' and line[n+1] == 'Q':
+            start = n - 1
             quest, n = find_next_quest(line, n)
-            return (quest, n, instr)
+            return (quest, n, start)
         else:
             if space:
                 space = False
                 instr = ''
             instr += char
             n += 1
-    return (None, len(line), 'None')
+    return (None, len(line), -1)
 
 def find_next_quest(line, n=0):
     # Read until '['
@@ -45,23 +122,23 @@ def find_next_quest(line, n=0):
     if not found:
         return (None, len(line))
     
-    quest = {'id':0, 'op':'none', 'name':''}
+    quest = Quest() # {'id':'', 'op':'none', 'name':''}
 
     # Read opcode
     assert(line[n] == 'Q')
-    quest['op'] = line[n+1]
+    quest.op = line[n+1]
     n += 2
 
     # Read quest id
-    id = ''
-    while line[n].isdigit():
-        id = id + line[n]
+    quest.id = ''
+    while line[n] != ' ':
+        quest.id = quest.id + line[n]
         n += 1
-    quest['id'] = int(id)
     n += 1 # skip space
 
+    quest.name = ''
     while line[n] != ']':
-        quest['name'] += line[n]
+        quest.name += line[n]
         n += 1
 
     return (quest, n+1)
@@ -91,9 +168,12 @@ def process_file(file, route):
                 break
             process_line(line.rstrip(), route, linenum)
     print()
+    # print('Questlog: ')
+    # print_questlog(route)
+    print()
 
 def scan_files():
-    route = {'accepted':[], 'completed':[], 'finished':[]}
+    route = Route() # {'accepted':[], 'completed':[], 'finished':[]}
     files = []
     for file in glob.glob('*.lua'):
         wds = file.split()
@@ -103,6 +183,15 @@ def scan_files():
     files = sorted(files)
     for file in files:
         process_file(file, route)
+    print(route.completed)
+
+class Route:
+    def __init__(self):
+        self.accepted = []
+        self.completed = []
+        self.finished = []
+class Quest:
+    pass
 
 def main():
     scan_files()
