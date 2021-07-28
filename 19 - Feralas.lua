e6cee98697af5dba38fd696c29d04c3b4e479390
Andreas Ruden <andreasruden91@gmail.com>
2021-07-28 17:28:19 +0200

    Work on Feralas

--- 19 - Feralas.lua
+++ 19 - Feralas.lua
@@ -23,7 +23,7 @@ Guidelime.registerGuide(
   -- Turn In: [51] The Sunken Temple -> [51] The Stone Circle (chest in Ratchet, then back to center of Tanaris)
 -- Feralas:
     -- Complete: [44] Screecher Spirits
-  -- (REQUIRES 2 elixir of fortitude) Accept: [47] An Orphan Looking For a Home -> Leads to [47] The Newest Member of the Family
+  -- (REQUIRES 2 elixir of fortitude) Accept: [47] An Orphan Looking For a Home (requires delivering Dolling Justice to Tyrande in Darnassus first) -> Leads to [47] The Newest Member of the Family
   -- Accept: [47] In Search of Knowledge -> Unlocks: (via darnassus) [48] The Borrower
     -- Accept & Complete: [50] The Stave of Equinex
   -- Accept: [51] The Sunken Temple (req 46)
@@ -38,7 +38,7 @@ Turn in [QT1190 Keeping Pace].\\Accept [QA1194 Rizzle's Schematics].
 Turn in [QT1194 Rizzle's Schematics].
 
 -- Gadgetzan
-Turn in [QT1188 Safety First].\\Accept [QA1189 Safety First].
+Turn in [QT1188 Safety First]. Skip follow-up for now. -- XXX: This is not worth to turn in XP wise, so we'll turn it in when we're going to shimmering flats anyway.
 Turn in [QT2864 Tran'rek]. Skip follow-up.
 Accept [QA2781 WANTED: Caliph Scorpidsting] and [QA2875 WANTED: Andre Firebeard].
 Accept [QA3022 Handle With Care].
@@ -46,25 +46,83 @@ Set hearthstone in [S Gadgetzan].
 Accept [QA1707 Water Pouch Bounty].
 Accept [QA1690 Wastewander Justice].
 
--- Steamwheedle Port
+-- Wastewanders
 [OC] If you find the Distress Beacon, save it.
-Complete [QC2781 WANTED: Caliph Scorpidsting].
-Complete [QC1690 Wastewander Justice] and [QC1707 Water Pouch Bounty].
+Complete [QC1690 Wastewander Justice].
+Head back and turn in [QT1690 Wastewander Justice].\\Accept [QA1691 More Wastewander Justice].
+Complete [QC2781 WANTED: Caliph Scorpidsting], [QC1691 More Wastewander Justice] and [QC1707 Water Pouch Bounty].
+
+-- Steamwheedle Port
+Accept [QA3520 Screecher Spirits].
 Turn in [QT2872 Stoley's Debt].\\Accept [QA2873 Stoley's Shipment].
 Accept [QA8366 Southsea Shakedown].
 Accept [QA8365 Pirate Hats Ahoy!]
 
 -- Pirates
+[OC] Open Pirate's Footlocker for chance at bonus quests.
 Complete [QC2875 WANTED: Andre Firebeard].
 In the house, second floor, complete [QC2873 Stoley's Shipment].
 Complete [QC8366 Southsea Shakedown] and [QC8365 Pirate Hats Ahoy!]
-Turn in [QT2873 Stoley's Shipment].\\Accept [QA2874 Deliver to MacKinley].
-Turn in [QT2875 WANTED: Andre Firebeard] and [QT8366 Southsea Shakedown].
-Turn in [QT8365 Pirate Hats Ahoy!]
-Accept [QA3520 Screecher Spirits].
-Deathwarp or run to gadgetzan. Turn in [QT1707 Water Pouch Bounty].
-Turn in [QT1690 Wastewander Justice] and [QT2781 WANTED: Caliph Scorpidsting].
+Grind pirates until [XP45-20200]. XXX: If this is far off then it'll be better to turn in quest at steamwheedle port, instead of hearthstoning out! Need to consider next 2 hearthstones in that case.
+[OC] Three map fragments gives [45] Cruego's Gold, which is completed at the southern tip of Tanaris. It gives no XP so we skip it in the guide.
+Hearthstone to [H Gadgetzan].
+Turn in [QT1707 Water Pouch Bounty].
+Turn in [QT2781 WANTED: Caliph Scorpidsting] and [QT1691 More Wastewander Justice].
 Fly to [F Feathermoon].
+
+-- Feathermoon
+Accept [QA2821 The Mark of Quality].
+Accept [QA2866 The Ruins of Solarsal].
+Accept [QA4124 The Missing Courier].
+Accept [QA2982 The High Wilderness].
+Turn in [QT4124 The Missing Courier].\\Accept follow-up [QA4125 The Missing Courier].
+Turn in [QT2866 The Ruins of Solarsal].\\Accept [QA2867 Return to Feathermoon Stronghold].
+Turn in [QT2867 Return to Feathermoon Stronghold].\\Accept [QA3130 Against the Hatecrest].
+Turn in [QT3130 Against the Hatecrest].\\Accept [QA2869 Against the Hatecrest].
+
+-- The Forgotten Coast & Isle of Dread
+[OC] Save distress beacon if you find it.
+[OC] If the boat is here, take it to the shore. Otherwise swim. -- XXX: Actually time the difference!
+Accept [QA7003 Zapped Giants] and [QA7721 Fuel for the Zapping].
+[OC] During the next step. If you spot a Screecher, kill them & use the bramble for progress on [QC3250 Screecher Spirits].
+Complete [QC7003 Zapped Giants] and [QC7721 Fuel for the Zapping], ending up in the south for the next quest.
+Turn in [QT4125 The Missing Courier].\\Accept [QA4127 Boat Wreckage].
+-- XXX: Ideally we would swim across to the southern island here, but unless you go even further south you'll die to fatigue. And going that far south seems slower than running back and taking the boat.
+Turn in [QT7003 Zapped Giants] and [QT7721 Fuel for the Zapping].
+Take the boat and jump off at the southern island to complete [QC2869 Against the Hatecrest]. (Also drops on northern island, which drop rate is better might need checking.)
+Swim to the northern island and turn in [QT2869 Against the Hatecrest].\\Accept [QA2870 Against Lord Shalzaru].
+Turn in [QT4127 Boat Wreckage].\\Accept [QA4129 The Knife Revealed].
+Turn in [QT4129 The Knife Revealed].\\Accept [QA4130 Psychometric Reading].
+Turn in [QT4130 Psychometric Reading].\\Accept [QA4131 The Woodpaw Gnolls].
+
+-- Central Feralas
+[OC] Save distress beacon if you find it.
+Take the boat or swim to shore. Finish up [QC3250 Screecher Spirits] by the beach. [G46.6,47.9]
+Accept [QA2969 Freedom for All Creatures].
+Open the big cage and protect some of the sprites to complete [QC2969 Freedom for All Creatures].
+Turn in [QT2969 Freedom for All Creatures].
+Accept [QA2970 Doling Justice].
+Complete [QC2970 Doling Justice].
+Turn in [QT2970 Doling Justice].\\Accept follow-up [QA2972 Doling Justice].
+Turn in [QT4131 The Woodpaw Gnolls].\\Accept [QA4135 The Writhing Deep].
+Enter the [G73.578,64.088 middle entrance] (not the bottom ones) of the insect hive. Follow the path to the right and turn in [QT4135 The Writhing Deep].\\Accept [QA4265 Freed from the Hive].
+Wait for him to monologue, which will complete [QC4265 Freed from the Hive].
+Complete [QC2982 The High Wilderness].
+Collect a [CI8564,1 Hippogryph Egg] [G58.17,75.93 here].
+Complete [QC2821 The Mark of Quality].
+If not yet aquired, grind Gordunni (1.1% on wowhead) or Yeti (0.8%) to find [CI8705,1 OOX-22/FE Distress Beacon].
+From the beacon, accept [QA2766 Find OOX-22/FE!]
+Grind around this area until [XP46-XXX]. (Needed for sunken temple quest)
+Turn in [QT2766 Find OOX-22/FE!]\\Accept escort [QA2767 Rescue OOX-22/FE!]
+Complete [QT2767 Rescue OOX-22/FE!]
+Take the boat and jump off at Isle of Dread. Head into the cave and go straight forward & up to run into Lord Shalzaru. Complete [QC2870 Against Lord Shalzaru].
+Swim back to Feathermoon and turn in [QT2870 Against Lord Shalzaru].\\Accept [QA2871 Delivering the Relic].
+Turn in [QT2871 Delivering the Relic].
+Turn in [QT2821 The Mark of Quality].
+Turn in [QT2982 The High Wilderness].\\If you're Lv.46 accept 
+Turn in [QT4265 Freed from the Hive].\\Accept [QA4266 A Hero's Welcome].
+Turn in [QT4266 A Hero's Welcome].\\Accept [QA4267 Rise of the Silithid].
+If not already accepted, accept [QA3445 The Sunken Temple].
 
 -- Feathermoon
 Accept [QA2939 In Search of Knowledge].
@@ -80,13 +138,15 @@ Turn in [QT2940 Feralas: A History].\\Accept [QA2941 The Borrower].
 From the AH, purchase [CI3825,2 Elixir of Fortitude] or skip a questline later.
 Hearthstone to [H Gadgetzan].
 
--- Feathermoon
--- The Mark of Quality
--- The Ruins of Solarsal
-Accept [QA4124 The Missing Courier].
--- In Search of Knowledge
--- The High Wilderness
-Turn in [QT4124 The Missing Courier].\\Accept follow-up [QA4125 The Missing Courier].
+-- Gadgetzan
+Turn in [QT2941 The Borrower].
+
+-- Steamwheedle Port
+Turn in [QT2873 Stoley's Shipment].\\Accept [QA2874 Deliver to MacKinley].
+If you found the Ship Schedule, accept [QA2876 Ship Schedules] and turn in [QT2876 Ship Schedules].
+Turn in [QT2875 WANTED: Andre Firebeard] and [QT8366 Southsea Shakedown].
+Turn in [QT8365 Pirate Hats Ahoy!]
+Turn in [QT3520 Screecher Spirits]. Skip follow-up.
 
 -- Quest notes:
 -- The ruins of Solarsal is 6 quests, that all take place on the two western islands.
