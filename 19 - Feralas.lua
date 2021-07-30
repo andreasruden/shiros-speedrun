8d983f81bea4b0ef79f476728fe057698e706ce1
Andreas Ruden <andreasruden91@gmail.com>
2021-07-30 21:21:28 +0200

    Finish tanaris and head towards Hinterlands

--- 19 - Feralas.lua
+++ 19 - Feralas.lua
@@ -27,13 +27,15 @@ Guidelime.registerGuide(
   -- Accept: [47] In Search of Knowledge -> Unlocks: (via darnassus) [48] The Borrower
     -- Accept & Complete: [50] The Stave of Equinex
   -- Accept: [51] The Sunken Temple (req 46)
+ 
+XXX: Silk/Mageweave donation
 
 -- Shimmering Flats
 Turn in [QT1117 Rumors for Kravel].\\Accept [QA1118 Back to Booty Bay].
 Turn in [QT1137 News for Fizzle].
 Turn in [QT1187 Razzeric's Tweaking].\\Accept [QA1188 Safety First].
 Accept [QA1190 Keeping Pace].
-Use [G79.82,77.03 Zamek] to create a distraction [QT1191 Zamek's Distraction]. -- !FinishWithoutAccept
+Use [G79.82,77.03 Shimmering Flats] Zamek to create a distraction [QT1191 Zamek's Distraction]. -- !FinishWithoutAccept
 Turn in [QT1190 Keeping Pace].\\Accept [QA1194 Rizzle's Schematics].
 Turn in [QT1194 Rizzle's Schematics].
 
@@ -72,6 +74,7 @@ Fly to [F Feathermoon].
 
 -- Feathermoon
 Accept [QA2821 The Mark of Quality].
+Set hearthstone in [S Feathermoon Stronghold]. XXX: We can most likely use it an extra time during the Feralas part, and not just save it for Darnassus.
 Accept [QA2866 The Ruins of Solarsal].
 Accept [QA4124 The Missing Courier].
 Accept [QA2982 The High Wilderness].
@@ -97,7 +100,7 @@ Turn in [QT4130 Psychometric Reading].\\Accept [QA4131 The Woodpaw Gnolls].
 
 -- Central Feralas
 [OC] Save distress beacon if you find it.
-Take the boat or swim to shore. Finish up [QC3250 Screecher Spirits] by the beach. [G46.6,47.9]
+Take the boat or swim to shore. Finish up [QC3250 Screecher Spirits] by the beach. [G46.6,47.9 Feralas]
 Accept [QA2969 Freedom for All Creatures].
 Open the big cage and protect some of the sprites to complete [QC2969 Freedom for All Creatures].
 Turn in [QT2969 Freedom for All Creatures].
@@ -105,10 +108,10 @@ Accept [QA2970 Doling Justice].
 Complete [QC2970 Doling Justice].
 Turn in [QT2970 Doling Justice].\\Accept follow-up [QA2972 Doling Justice].
 Turn in [QT4131 The Woodpaw Gnolls].\\Accept [QA4135 The Writhing Deep].
-Enter the [G73.578,64.088 middle entrance] (not the bottom ones) of the insect hive. Follow the path to the right and turn in [QT4135 The Writhing Deep].\\Accept [QA4265 Freed from the Hive].
+Enter the [G73.578,64.088 Feralas] middle entrance (not the bottom ones) of the insect hive. Follow the path to the right and turn in [QT4135 The Writhing Deep].\\Accept [QA4265 Freed from the Hive].
 Wait for him to monologue, which will complete [QC4265 Freed from the Hive].
 Complete [QC2982 The High Wilderness].
-Collect a [CI8564,1 Hippogryph Egg] [G58.17,75.93 here].
+Collect a [CI8564,1 Hippogryph Egg] [G58.17,75.93 Feralas] here.
 Complete [QC2821 The Mark of Quality].
 If not yet aquired, grind Gordunni (1.1% on wowhead) or Yeti (0.8%) to find [CI8705,1 OOX-22/FE Distress Beacon].
 From the beacon, accept [QA2766 Find OOX-22/FE!]
@@ -119,13 +122,14 @@ Take the boat and jump off at Isle of Dread. Head into the cave and go straight 
 Swim back to Feathermoon and turn in [QT2870 Against Lord Shalzaru].\\Accept [QA2871 Delivering the Relic].
 Turn in [QT2871 Delivering the Relic].
 Turn in [QT2821 The Mark of Quality].
-Turn in [QT2982 The High Wilderness].\\If you're Lv.46 accept 
+Turn in [QT2982 The High Wilderness].\\If you're Lv.46 accept [QA3445 The Sunken Temple].
 Turn in [QT4265 Freed from the Hive].\\Accept [QA4266 A Hero's Welcome].
 Turn in [QT4266 A Hero's Welcome].\\Accept [QA4267 Rise of the Silithid].
 If not already accepted, accept [QA3445 The Sunken Temple].
 
 -- Feathermoon
 Accept [QA2939 In Search of Knowledge].
+Seth hearthstone in
 Fly to [F Rut'theran Village]. This flight takes 9 minutes.
 
 -- Darnassus
@@ -133,20 +137,87 @@ Turn in [QT3022 Handle With Care].\\Accept [QA3661 Favored of Elune?]
 Turn in [QT2939 In Search of Knowledge].
 Accept [QA2940 Feralas: A History].
 Turn in [QT2940 Feralas: A History].\\Accept [QA2941 The Borrower].
+[T] Learn Lv.44 & 46 skills.
 [A Nightelf] Purchase a mount if you have not done so already.
-[T] Learn Lv.44 skills.
 From the AH, purchase [CI3825,2 Elixir of Fortitude] or skip a questline later.
-Hearthstone to [H Gadgetzan].
+At the upper floor of Temple of the Moon, turn in [QT4267 Rise of the Silithid].
+Turn in [QT2972 Doling Justice].
+
+-- Feralas
+Hearthstone to [H Feathermoon Stronghold].
+[OC] If you did not get 2 Elixir of Fortitude, fly straight to Gadgetzan.
+Run to Kindal and accept [QA3841 An Orphan Looking For a Home]. XXX: Is flying to Thalanaar better than running from feathermoon?
+Run east to Thalanaar and fly to [F Gadgezan].
 
 -- Gadgetzan
-Turn in [QT2941 The Borrower].
+Accept [QA2605 The Thirsty Goblin].
+Set hearthstone in [S Gadgetzan].
+Accept [QA5863 The Dunemaul Compound].
+Turn in [QT2741 The Super Egg-O-Matic]. -- !FinishWithoutAccept
+[OC] Can turn in the resulting egg you got to the gnome, only gives rep though.
+Turn in [QT2941 The Borrower].\\Accept [QA2944 The Super Snapper FX].
+Accept [QA3362 Thistleshrub Valley].
+Accept [QA1189 Safety First].
+Accept [QA992 Gadgetzan Water Survey].
+Jump over the wall and head west to complete [QC992 Gadgetzan Water Survey].
+Turn in [QT992 Gadgetzan Water Survey].\\Accept [QA82 Noxious Lair Investigation].
 
+-- First round of Tanaris
+Turn in [QT3445 The Sunken Temple].\\Accept [QA3444 The Stone Circle] and [QA3161 Gahz'ridian].
+Complete [QC82 Noxious Lair Investigation].
+Complete [QC5863 The Dunemaul Compound]. [G41.5,57.8 Tanaris]
+At Eastmoon Ruins (47,66) and Southmoon Ruins (40,73) complete [QC3161 Gahz'ridian]. [G47,66 Tanaris]
+Complete [QC2605 The Thirsty Goblin] and [QC3362 Thistleshrub Valley].
+[O] We're skipping Tooga, since a player can occupy her for up to 30 minutes it's unreliable to find her even for an escort quest.
+Hearthstone to [H Gadgetzan]. If it's not up you'll have a long run :-(.
+Turn in [QT2605 The Thirsty Goblin].\\Accept [QA2606 In Good Taste].
+Turn in [QT82 Noxious Lair Investigation].
+Accept [QA10 The Scrimshank Redemption].
+Turn in [QT2606 In Good Taste].\\Accept [QA2641 Sprinkle's Secret Ingredient].
+Turn in [QT3362 Thistleshrub Valley].
+Turn in [QT5863 The Dunemaul Compound].
+
+-- Second round of Tanaris
+Turn in [QT3161 Gahz'ridian].
+Enter the hive and fight your way to the central room to complete [QC10 The Scrimshank Redemption].
+You should surely have the distress beacon at this point, so accept [QA351 Find OOX-17/TN!].
+Turn in [QT351 Find OOX-17/TN!].\\Accept the escort [QA648 Rescue OOX-17/TN!]
+Complete the escort [QC648 Rescue OOX-17/TN!]
 -- Steamwheedle Port
 Turn in [QT2873 Stoley's Shipment].\\Accept [QA2874 Deliver to MacKinley].
 If you found the Ship Schedule, accept [QA2876 Ship Schedules] and turn in [QT2876 Ship Schedules].
 Turn in [QT2875 WANTED: Andre Firebeard] and [QT8366 Southsea Shakedown].
 Turn in [QT8365 Pirate Hats Ahoy!]
 Turn in [QT3520 Screecher Spirits]. Skip follow-up.
+-- Gadgetzan
+Turn in [QT10 The Scrimshank Redemption].\\Accept [QA110 Insect Part Analysis].
+Turn in [QT110 Insect Part Analysis].\\Accept follow-up [QA113 Insect Part Analysis].
+Turn in [QT113 Insect Part Analysis].\\Accept [QA162 Rise of the Silithid].
+
+-- Shimmering Flats
+Turn in [QT1189 Safety First].
+Turn in [QT3841 An Orphan Looking For a Home].\\Accept [QA3842 A Short Incubation].
+Turn in [QT3842 A Short Incubation].\\Accept [QA3843 The Newest Member of the Family].
+XXX: Back to Booty Bay was acceptable here??? Did I not take that earlier? When can we turn that in?
+Run back to Gadgetzan and fly to [F Theramore].
+XXX: Not worth going by arathi to do Sunken Treasure, is it?
+Fly to [F Southshore].
+Run to [G86.2,30.1,25 Hillsbrad Foothills] Hinterland's Entrance.
+Accept [QA9469 Featherbeard's Endorsement], [QA9470 A Gesture of Goodwill] and [QA9471 Preying on the Predators].
+In the keep, turn right and accept [QA2877 Skulk Rock Clean-up] and [QA2880 Troll Necklace Bounty].
+Head straight and down the stairs to the bottom of the keep to turn in [QT3843 The Newest Member of the Family].\\Accept [QA4297 Food for Baby].
+Head back out of the keep and then follow the path that goes up and turn in [QT1449 To The Hinterlands].\\Accept [QA1450 Gryphon Master Talonaxe].
+Continue up and get FP [P Aerie Peak].
+Turn in [QT1450 Gryphon Master Talonaxe].\\Accept [QA1451 Rhapsody Shindigger] and [QA2988 Witherbark Cages].
+
+
+XXX: This is not done yet
+-- In Ratchet
+Loot the chest outside of Liv's shop to complete [QC3444 The Stone Circle].
+
+-- In Booty Bay
+Run towards the inn, in the building just before, turn in [QT2874 Deliver to MacKinley].
+In the inn, turn in [QT1118 Back to Booty Bay].
 
 -- Quest notes:
 -- The ruins of Solarsal is 6 quests, that all take place on the two western islands.
