3cbeb7cbf67600725a545f31bfcaa3bf8c9ac682
Andreas Ruden <andreasruden91@gmail.com>
2021-08-11 14:01:23 +0200

    Work on Un'Goro Crater

---
+++ 24 - Blasted Lands.lua
@@ -0,0 +1,32 @@
+Guidelime.registerGuide(
+[[
+
+[D Shiros 1-58 Speedrun for alliance (patch 2.3+)]
+[GA Alliance]
+[N 50-51 Blasted Lands]
+[NX 51-52 Un'Goro Crater]
+
+Fly to [F Stormwind].
+[O][T] While there, train Lv. 50 skills.
+Turn in [QT1469 Rhapsody's Tale].\\Accept [QA1475 Into The Temple of Atal'Hakkar].
+From the AH, purchase a [CI10561,1 Mithril Casing].
+Fly to [F Nethergarde Keep].
+
+XXX: Should we swap order? In case we're not Lv.50
+-- Nethergarde Keep
+Inside the tower, turn in [QT2990 Thadius Grimshade]. Skip follow-up.
+Accept [QA2783 Petty Squabbles].
+
+-- Swamp of Sorrows
+Turn in [QT2783 Petty Squabbles].\\Accept [QA2801 A Tale of Sorrow].
+Talk to the fallen hero to complete [QC2801 A Tale of Sorrow].
+Turn in [QT2801 A Tale of Sorrow].\\Accept [QA2681 The Stones That Bind Us].
+Head to the temple in the middle of the lake, and from the start of the temple to *before* the instance portal complete [QC1475 Into The Temple of Atal'Hakkar][G69.8,53.8 Swamp of Sorrows].
+
+-- Blasted Lands
+Run back to Blasted Lands and accept [QA2601 The Basilisk's Bite] and [QA2603 Vulture's Vigor].
+Accept [QA2581 Snickerfang Jowls], [QA2583 A Boar's Vitality] and [QA2585 The Decisive Striker].
+In total, we will need to collect 6 [CI8392,6 Blasted Boar Lung], 6 [CI8393,6 Scorpok Pincer], 11 [CI8394,11 Basilisk Brain], 5 [CI8391,5 Snickerfang Jowl] and 14 [CI8396,14 Vulture Gizzard].
+XXX: Routing + Advice
+
+]], 'Shiros Speedrun')
---
+++ 25 - Ungoro.lua
@@ -0,0 +1,79 @@
+Guidelime.registerGuide(
+[[
+
+[D Shiros 1-58 Speedrun for alliance (patch 2.3+)]
+[GA Alliance]
+[N 52-54 Un'Goro Crater]
+[NX X-X X]
+
+Hearth to [H Ratchet].
+
+XXX: A Mangled Journal
+
+-- Gadgetzan
+Fly to [F Gadgetzan].
+Turn in [QT4493 March of the Silithid].\\Accept [QA4496 Bungle in the Jungle].
+Turn in [QT2641 Sprinkle's Secret Ingredient].
+Accept [QA4504 Super Sticky].
+Turn in [QT1119 Zanzil's Mixture and a Fool's Stout].\\Accept [QA1120 Get the Gnomes Drunk]. -- XXX: This one has lost 60% xp at Lv.52, should consider putting it ealier somehow. Maybe artifically going via ratchet/bb to Hinterlands is an okay time/xp-trade?
+Turn in [QT1120 Get the Gnomes Drunk].
+Accept [QA1122 Report Back to Fizzlebub]. XXX: Do we do this?
+Accept [QA2661 Delivery for Marin].
+Turn in [QT2661 Delivery for Marin].\\Accept [QA2662 Noggenfogger Elixir].
+Turn in [QT2662 Noggenfogger Elixir].
+
+-- To Un'Goro
+Turn in [QT3444 The Stone Circle].
+The path into Un'Goro Crater is [G27.01,56.63 Tanaris] here.
+[O] You will need to *aggressively* gather the power crystals: 7x[CI11184,7 Blue Power Crystal], 7x[CI11185,7 Green Power Crystal], 7x[CI11186,7 Red Power Crystal] and 7x[CI11188,7 Yellow Power Crystal]. We need 7 of each *asap*. They hide around the big trees and along the mountains.
+We also *need* the 5 [QC4496,2 Un'Goro Soil][O]. They come from killing + looting on the ground.
+Accept [QA4289 The Apes of Un'Goro] and [QA4290 The Fare of Lar'korwi].
+[O] We'll trace a rough path, use it to grind some raptors and loot crystals.
+[G69.89,68.88,25 Un'Goro Crater]
+[G72.38,63.57,25 Un'Goro Crater]
+[G75.01,60.87,25 Un'Goro Crater]
+[G72.43,52.82,25 Un'Goro Crater]
+Loot the big dinosaur to complete [QC4290 The Fare of Lar'korwi].
+[G71.06,64.10,20 Un'Goro Crater]
+[G71.06,64.10,20 Un'Goro Crater]
+[G70.52,70.25,20 Un'Goro Crater]
+Run over the stone & waterfall [G71.447032,74.470466,7 Un'Goro Crater] here to get to the NPC.
+Turn in [QT4290 The Fare of Lar'korwi].\\Accept [QA4291 The Scent of Lar'korwi].
+Kill raptors to complete [QC4291 The Scent of Lar'korwi][O] on the way.
+[O] We'll trace a rough path, use it to grind raptors for the *glands* and loot *crystals*.
+[G66.80,72.97,25 Un'Goro Crater]
+[G65.68,77.49,25 Un'Goro Crater]
+[G61.54,75.98,25 Un'Goro Crater]
+[G57.76,78.21,25 Un'Goro Crater]
+[G55.61,72.18,25 Un'Goro Crater]
+Grind Gorishi mobs until the [QC4496,1 Gorishi Scent Gland][G51.6,77.2 Un'Goro Crater] drops.
+[G57.91,77.12,25 Un'Goro Crater]
+[G61.67,70.81,25 Un'Goro Crater]
+From the Wrecked Raft, accept [QA3844 It's a Secret to Everybody].
+Turn in [QT3844 It's a Secret to Everybody].\\Accept follow-up [QA3845 It's a Secret to Everybody].
+Open the item *A Small Pack* to complete [QC3845 It's a Secret to Everybody].
+Finish up [QC4291 The Scent of Lar'korwi][G66.9,72.9 Un'Goro Crater] in this area while looking for crystals.
+[G70.06,68.26,20 Un'Goro Crater]
+Run over the stone & waterfall [G71.447032,74.470466,7 Un'Goro Crater] here to get to the NPC.
+Turn in [QT4291 The Scent of Lar'korwi].\\Accept [QA4292 The Bait for Lar'korwi].
+
+Complete [QC4289 The Apes of Un'Goro][G63.7,16.6 Un'Goro Crater].
+XXX: Power crystals before Marshal's Refuge?
+As you head towards Marshal's Refuge, make sure to complete [QC4496,2 Un'Goro Soil][G46.5,13.7 Un'Goro Crater].
+
+-- Marshal's Refuge
+Accept [QA4243 Chasing A-Me 01].
+Accept [QA4503 Shizzle's Flyer].
+Accept [QA4141 Muigin and Larion].
+Accept [QA4492 Lost!]
+Accept [QA4501 Beware of Pterrordax].
+Accept [QA3882 Roll the Bones].
+Accept [QA3883 Alien Ecology].
+Accept [QA3881 Expedition Salvation].
+Get FP [P Un'Goro Crater].
+Fly to [F Gadgetzan].
+Turn in [QT4496 Bungle in the Jungle].\\Accept [QA4507 Pawn Captures Queen].
+Set hearthstone in [S Gadgetzan].
+Fly to [F Un'Goro Crater].
+
+]], 'Shiros Speedrun')
