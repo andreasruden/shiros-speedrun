67b99610824796df63401de65e6861920e2bd465
Andreas Ruden <andreasruden91@gmail.com>
2021-08-19 08:32:02 +0200

    Azshara WIP

---
+++ 23 - Azshara.lua
@@ -0,0 +1,50 @@
+Guidelime.registerGuide(
+[[
+
+[D Shiros 1-58 Speedrun for alliance (patch 2.3+)]
+[GA Alliance]
+[N 50-51 Azshara]
+[NX 51-52 Feralas]
+
+XXX: Get quest for all classes.
+
+Fly to [F Astranaar].
+Set hearthstone in [S Astranaar].
+Fly to [F Forest Song].
+Ride to Azshara and get FP [P Talrendis Point].
+Accept [QA5536 A Land Filled with Hatred] and [QA5535 Spiritual Unrest].
+Complete [QC5536 A Land Filled with Hatred] and [QC5535 Spiritual Unrest].
+Turn in [QT5536 A Land Filled with Hatred] and [QT5535 Spiritual Unrest].
+
+[A rogue][O]Do *NOT* kill the Timbermaw furbolgs!
+[A rogue]Try pickpocketing the Timbermaw Shaman [G38.2,32.6 Azshara] here.
+[A rogue]If that one didn't give it, complete [QC8234 Sealed Azure Bag][G45.5,25.1 Azshara].
+[A rogue]Use the [G28.1,50.1 Azshara] transporter to get above.
+[A rogue]Turn in [QC8234 Sealed Azure Bag].\\Accept	[QA8235 Encoded Fragments].
+Accept [QA3601 Kim'jael Indeed!]
+[A rogue]Pickpocket then kill slimes to complete [QA8235 Encoded Fragments][G73.3,26.9 Azshara]. Can also drop from Mistwing Ravagers. The droprate on this seems a bit questionable.
+Loot Equipment Boxes to complete [QA3601 Kim'jael Indeed!]
+Turn in [QA3601 Kim'jael Indeed!]\\Accept [QA5534 Kim'jael's "Missing" Equipment].
+[A rogue]Use the [G28.1,50.1 Azshara] transporter to get above.
+[A rogue]Turn in [QT8235 Encoded Fragments]. Skip follow-up.
+Complete [QA5534 Kim'jael's "Missing" Equipment] and [QC3449 Arcane Runes].
+Turn in [QA5534 Kim'jael's "Missing" Equipment].
+
+XXX: Pick up Xiggs quest from IF
+Turn in [QT3449 Arcane Runes].\\Accept [QA3461 Return to Tymor].
+Hearth to [H Astranaar].
+
+-- Booty Bay
+Fly to [F Ratchet].
+Set hearthstone in [S Ratchet].
+Loot the chest outside of Liv's shop to complete [QC3444 The Stone Circle].
+Accept [QA4502 Volcanic Acitivy].
+Take the [G63.7,38.7 The Barrens] boat to Booty Bay.
+[G27.7767,76.6705 Stranglethorn vale]
+[G28.2333,76.1299 Stranglethorn vale]
+Turn in [QT2767 Rescue OOX-22/FE!], [QT648 Rescue OOX-17/TN!] and [QT836 Rescue OOX-09/HL!]
+Turn in [QT3721 An OOX of Your Own]. -- !FinishWithoutAccept
+Run towards the inn, in the building just before, turn in [QT2874 Deliver to MacKinley].
+In the inn, turn in [QT580 Whiskey Slim's Lost Grog].
+
+]], 'Shiros Speedrun')
