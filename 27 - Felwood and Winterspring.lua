2e7cccbe507f8abb5df04f58fa17234b37bc479c
Andreas Ruden <andreasruden91@gmail.com>
2021-08-19 08:32:13 +0200

    Felwood + WS WIP

---
+++ 27 - Felwood and Winterspring.lua
@@ -0,0 +1,81 @@
+Guidelime.registerGuide(
+[[
+
+[D Shiros 1-58 Speedrun for alliance (patch 2.3+)]
+[GA Alliance]
+[N 55-57 Felwood]
+[NX X-X X]
+
+-- Emerald Song
+Accept [QA5155 Forces of Jaedenar].
+Accept [QA4421 The Corruption of the Jadefire].
+Accept [QA5156 Verifying the Corruption].
+Accept [QA5249 To Winterspring!]
+
+-- Heading to northern Felwood
+[OC] Timbermaw Ally is not worth doing at this level. Lost too much xp.
+Accept [QA4101 Cleansing Felwood].
+Head all the way in the back of the camp and kill [QC4421,4 Xavathras].
+Finish up [QC4421 The Corruption of the Jadefire].
+Go around the mountain and complete [QC5155 Forces of Jaedenar].
+Complete [QC5156 Verifying the Corruption].
+[G49.5,35.3,25 Felwood]
+[G55.0,22.1,50 Felwood]
+Complete [QC4101 Cleansing Felwood][G54.7,16.7 Felwood].
+Get FP [P Talonbranch Glade].
+Accept [QA8461 Deadwood of the North].
+Complete [QC8461 Deadwood of the North].
+Turn in [QT8461 Deadwood of the North].\\Accept [QA8465 Speak to Salfa].
+XXX: Not worth turning in Rabine Saturna in moonglade, unless we want the follow-up that goes to Silithus.
+
+-- Winterspring
+Head through to Winterspring and turn in [QT8465 Speak to Salfa].\\Accept [QA8464 Winterfall Activity].
+Loot feathers on the ground for progress on [QC978 Moontouched Wildkin][O].
+Turn in [QC3908 It's a Secret to Everybody].\\Accept [QC3908 The Videre Elixir] and [QA5082 Threat of the Winterfall].
+Turn in [QC980 The New Springs].\\Accept [QA4842 Strange Sources].
+North and east there are mobs for the quest. Complete [QC5082 Threat of the Winterfall][G40.1,43.8 Winterspring]. [L31,36 Winterspring]
+Farm the same mobs until you obtain [CI12771, 1 Empty Firewater Flask].
+Accept [QA5083 Winterfall Firewater].
+Turn in [QT5082 Threat of the Winterfall] and [QT5083 Winterfall Firewater].\\Accept [QA5084 Falling to Corruption].
+
+-- Felwood <-> Winterspring ping-pong
+Head through into Felwood and turn in [QT5084 Falling to Corruption].\\Accept [QA5085 Mystery Goo].
+Back to Winterspring and turn in [QT5085 Mystery Goo].\\Accept [QA5086 Toxic Horrors].
+Complete [QC5086 Toxic Horrors].
+Turn in [QT5086 Toxic Horrors].\\Accept [QA5087 Winterfall Runners].
+Complete [QC5087 Winterfall Runners].
+Turn in [QT5087 Winterfall Runners].\\Accept [QA5121 High Chief Winterfall].
+
+-- Everlook
+XXX: A Little Luck probably does not exist.
+Accept [QA6604 Enraged Wildkin] and [QA6606 A Little Luck].
+Sister Pamela.
+The Everlook Report and Duke Nicholas Zverenhoff.
+Accept [QA5054 Ursius of the Shardtooth].
+Turn in [QT6606 A Little Luck]. Luck Be With You.
+Accept [QA3783 Are We There, Yeti?]
+Get FP [P Everlook].
+
+-- Around Everlook (TODO: Later, after more felwood, or now?)
+[G63.4,38.3 Winterspring]
+Complete [QC3783 Are We There, Yeti?][G65.5,41.2 Winterspring]
+Head back past the FP and towards [G64.4,34.3,40 Winterspring] this path.
+[OC] The Highchief for the next step is Lv.59, guarded by one Lv.56 mob.
+Complete [QC8464 Winterfall Activity][G69.8,38.2 Winterspring] and [QC5121 High Chief Winterfall][G69.8,38.2 Winterspring].
+From the Crudely-written Log, accept [QA5123 The Final Piece].
+Complete [QC5054 Ursius of the Shardtooth].
+
+-- Starfall Village
+Turn in [QT5249 To Winterspring!]\\Accept [QA5244 The Ruins of Kel'Theril].
+Turn in [QT5244 The Ruins of Kel'Theril] and [QT6604 Enraged Wildkin].\\Accept [QA5245 Troubled Spirits of Kel'Theril] and [QA4861 Enraged Wildkin].
+
+-- Everlook
+Turn in [QT5054 Ursius of the Shardtooth].\\Accept [QA5055 Brumeran of the Chillwind].
+Turn in [QT3783 Are We There, Yeti?]\\Accept follow-up [QA977 Are We There, Yeti?]
+
+Finish up [QC978 Moontouched Wildkin].
+
+
+XXX: Get northern FP sometime
+
+]], 'Shiros Speedrun')
