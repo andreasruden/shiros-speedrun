a45d7c37c9a5117cd5d5baae038bec303d2bd7fa
Andreas Ruden <andreasruden91@gmail.com>
2021-08-20 09:33:35 +0200

    Start work on WPL & EPL

---
+++ 28 - Western and Eastern Plaguelands.lua
@@ -0,0 +1,72 @@
+Guidelime.registerGuide(
+[[
+
+[D Shiros 1-58 Speedrun for alliance (patch 2.3+)]
+[GA Alliance]
+[N 57-59 Western and Eastern Plaguelands]
+[NX X-X X]
+
+-- Menethil Harbor
+Take boat to Menethil Harbor.
+Set hearthstone in [S Deepwater Tavern][G10.7,60.9 Wetlands].
+
+-- Ironforge
+Fly to [F Ironforge].
+Turn in [QT4512 A Little Slime Goes a Long Way]. Take follow-up?
+
+-- Stormwind
+Take the [G73,50 Ironforge] tram to Stormwind.
+In the stormwind keep, accept [QA6182 The First and the Last].
+In the SI:7 house, turn in [QT6182 The First and the Last].\\Accept [QA6183 Honor the Dead].
+Turn in [QT6183 Honor the Dead].\\Accept [QA6184 Flint Shadowmore].
+Accept [QA5066 A Call to Arms: The Plaguelands!]
+Hearth to [H Deepwater Tavern].
+
+-- Chillwind Camp
+Fly to [F Chillwind Camp].
+Accept [QA9474 The Mark of the Lightbringer].
+Accept [QA5903 A Plague Upon Thee].
+Turn in [QT5066 A Call to Arms: The Plaguelands!]\\Accept [QA5092 Clear the Way].
+Accept [QA5405 Argent Dawn Commision].
+Turn in [QT5405 Argent Dawn Commision].
+Equip the insignia and save up to either: 20 minion, 10 invader or 1 corruptor *Scourgestone*.
+Turn in [QT6028 The Everlook Report].
+Turn in [QT6184 Flint Shadowmore].\\Accept [QA6185 The Eastern Plagues].
+
+-- Sorrow Hill
+Accept [QA5142 Little Pamela].
+Finish up [QC5092 Clear the Way].
+
+-- Chillwind Camp
+Turn in [QT5092 Clear the Way].\\Accept [QA5097 All Along the Watchtowers] and [QA5215 The Scourge Cauldrons].
+Turn in [QT5215 The Scourge Cauldrons].\\Accept [QA5216 Target: Felstone Field].
+
+-- Felstone Field
+Jump from here [G37.325116,70.890659 Western Plaguelands].
+[G37.292820,69.930118,2 Western Plaguelands]
+[G37.103596,69.224709 Western Plaguelands]
+Kill the cauldron lord to complete [QC5216 Target: Felstone Field].
+Turn in [QT5216 Target: Felstone Field].\\Accept [QA5217 Return to Chillwind Camp].
+On the second floor of the house, accept [QA5021 Better Late Than Never].
+In the barn, turn in [QT5021 Better Late Than Never].\\Accept follow-up [QA5022 Better Late Than Never].
+
+-- Chillwind Camp
+From here [G37.156075,69.719827 Western Plaguelands], hug the terrain to get over without dismounting.
+Turn in [QT5217 Return to Chillwind Camp].\\Accept [QA5219 Target: Dalson's Tears].
+
+-- Ruins of Andorhal & Dalson's Tears
+Use the Beacon Torch to complete [QC5097,1 Tower One Marked][G40,71 Western Plaguelands].
+[G39.6,68.4 Western Plaguelands]
+On the second floor, in the furthest room in the back, accept [QA4971 A Matter of Time].
+Complete [QC5097,2 Tower Two Marked][G42,66 Western Plaguelands].
+Complete [QC5097,3 Tower Three Marked][G44,63 Western Plaguelands].
+Use the Temporal Displacer next to a silo and kill parasites to complete [QC4971 A Matter of Time][G46,63 Western Plaguelands].
+Complete [QC5219 Target: Dalson's Tears].
+Turn in [QT5219 Target: Dalson's Tears].\\Accept [QA5220 Return to Chillwind Camp].
+Go inside the barn and turn in [QT5058 Mrs. Dalson's Diary].
+Kill a *Wandering Skeleton* to obtain [CI12738,1 Dalson Outhouse Key][G47.75,49.78 Western Plaguelands].
+Turn in [QT5059 Locked Away][G48.09,49.70 Western Plaguelands] at the outhouse.
+Kill Farmer Dalson, who spawns, to obtain [CI12739,1 Dalson Cabinet Key].
+Go to the second floor of the building and turn in [QT5060 Locked Away][G47.36,49.64 Western Plaguelands].
+
+]], 'Shiros Speedrun')
