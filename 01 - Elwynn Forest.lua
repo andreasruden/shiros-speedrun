4c8f7e102097b845c5d745993e7a60ff4fe05cd7
Andreas Ruden <andreasruden91@gmail.com>
2021-08-24 11:02:22 +0200

    Human starting area

--- 01 - Elwynn Forest.lua
+++ 01 - Elwynn Forest.lua
@@ -17,11 +17,11 @@ Complete [QC33 Wolves Across the Border] and [QC7 Kobold Camp Cleanup].
 Turn in [QT33 Wolves Across the Border].
 Turn in [QT7 Kobold Camp Cleanup].\\Accept [QA15 Investigate Echo Ridge].
 -- XXX: Every class.
-Accept [QA3103 Consecrated Letter].
-Turn in [QT3103 Consecrated Letter] and [T]train.
+Accept [QA3101 Consecrated Letter].
+Turn in [QT3101 Consecrated Letter] and [T]train.
 Accept [QA18 Brotherhood of Thieves].
-Complete [QC15 Investigate Echo Ridge].
-Complete [QC18 Brotherhood of Thieves].
+Complete [QC15 Investigate Echo Ridge] ending close to the river to the east.
+Cross the river and complete [QC18 Brotherhood of Thieves][G55,44 Elwynn Forest].
 Deathwarp back. *Deathwarp:* Die & ress at spirit healer. Spam /sit keybind to make dying quicker.
 Turn in [QT18 Brotherhood of Thieves].\\Accept [QA6 Bounty on Garrick Padfoot] and [QA3903 Milly Osworth].
 Turn in [QT15 Investigate Echo Ridge].\\Accept [QA21 Skirmish at Echo Ridge].
@@ -39,7 +39,6 @@ Turn in [QT3905 Grape Manifest].
 Accept [QA2158 Rest and Relaxation].
 
 -- Goldshire
-Die [G43.7,50.5 Elwynn Forest] here to deathwarp closer to goldshire.
 Turn in [QT54 Report to Goldshire].\\Accept [QA62 The Fargodeep Mine].
 In the inn, accept [QA60 Kobold Candles].
 Turn in [QT2158 Rest and Relaxation].
@@ -58,11 +57,10 @@ Turn in [QT86 Pie for Billy].\\Accept [QA84 Back to Billy].
 Turn in [QT111 Speak with Gramma].\\Accept [QA107 Note to William].
 
 -- Goldshire
-Deathwarp to Goldshire by dying to [G37.9,82.7 Elwynn Forest] Kobolds.
+Hearth to [H Lion's Pride Inn].
 Turn in [QT107 Note to William].\\Accept [QA112 Collecting Kelp].
 Complete [QC112 Collecting Kelp].
-Run or deathwarp to Goldshire (depending on where you finish?).
-Turn in [QT112 Collecting Kelp].\\Accept [QA114 The Escape].
+Run or deathwarp back (depending on side you finish), turn in [QT112 Collecting Kelp].\\Accept [QA114 The Escape].
 
 -- Fargodeep Mine
 Turn in [QT84 Back to Billy].\\Accept [QA87 Goldtooth].
@@ -81,24 +79,23 @@ Turn in [QT60 Kobold Candles].\\Accept [QA61 Shipment to Stormwind].
 
 -- Eastvale Logging Camp
 Complete [QC88 Princess Must Die!]
-Turn in [QT35 Further Concerns].\\Accept [QA37 Find the Lost Guards], [QA46 Bounty on Murlocs] and [QA52 Protect the Frontier].
+Turn in [QT35 Further Concerns].\\Accept [QA37 Find the Lost Guards] and [QA52 Protect the Frontier].
 Accept [QA83 Red Linen Goods].
 Accept [QA5545 A Bundle of Trouble].
-If you spot a [QC52,2 Young Forest Bear], kill it!
+If you spot a [QC52,2 Young Forest Bear][O], kill it!
 Kill 8 [QC52,1 Prowler][G79.6,62.1 Elwynn Forest] and complete [QC5545 A Bundle of Trouble][G79.6,62.1 Elwynn Forest]. If you finish the wood before the prowlers, move on and take them on the way.
-Turn in [QC37 Find the Lost Guards].\\Accept [QA45 Discover Rolf's Fate].
-Kill your way through the murloc camp and turn in [QT45 Discover Rolf's Fate] (can be kind of difficult!)\\Accept [QA71 Report to Thomas].
-Finish up [QC46 Bounty on Murlocs].
-Complete [QC83 Red Linen Goods][G74.5,51.9 Elwynn Forest].
+Turn in [QT37 Find the Lost Guards].\\Accept [QA45 Discover Rolf's Fate].
+Turn in [QT45 Discover Rolf's Fate] (can be kind of difficult, lots of chaining here!)\\Accept [QA71 Report to Thomas].
+Complete [QC83 Red Linen Goods][G74.5,51.9 Elwynn Forest]. XXX: Should we use the mobs at Princess farm instead? And do her at the same time.
+If you obtained the *Westfall Deed*, accept [QA184 Furlbrow's Deed]. Otherwise skip this step.
 Deathwarp to Eastvale Logging Camp.
 Turn in [QT5545 A Bundle of Trouble].
 Turn in [QT83 Red Linen Goods].
 If not finished, finish up [QC52 Protect the Frontier][G77.9,78.8 Elwynn Forest].
-Turn in [QT46 Bounty on Murlocs], [QT52 Protect the Frontier] and [QT71 Report to Thomas].\\Accept [QA39 Deliver Thomas' Report] and [QA109 Report to Gryan Stoutmantle].
+Turn in [QT52 Protect the Frontier] and [QT71 Report to Thomas].\\Accept [QA39 Deliver Thomas' Report].
 [OC]Time dying to when the quest finishes.
 Run into the mine to complete [QC76 The Jasperlode Mine].
 Deathwarp to Goldshire.
-Accept [1097 Elmore's Task]. XXX: Is this even worth accepting?
 Turn in [QT39 Deliver Thomas' Report]. Skip follow-up.
 Turn in [QT76 The Jasperlode Mine].\\Accept [QA239 Westbrook Garrison Needs Help!]
 
@@ -107,10 +104,80 @@ Turn in [QT87 Goldtooth].
 Turn in [QT88 Princess Must Die!]
 Accept [QA176 Wanted:  "Hogger"].
 Turn in [QT239 Westbrook Garrison Needs Help!]\\Accept [QA11 Riverpaw Gnoll Bounty].
-Hogger is still elite in TBC, if you're strong enough or can find someone nearby complete [QA176 Wanted:  "Hogger"]. Otherwise, abandon it.
-Complete [QC11 Riverpaw Gnoll Bounty].
-If you killed Hogger, deathwarp back to Goldshire and turn in [QT176 Wanted:  "Hogger"]. XXX: If we go on to Westfall, then is it even worth doing this? What's the xp/hr compared to the zone xp/hr? Do we need to go on to westfall to get Lv.12?
-Turn in [QT11 Riverpaw Gnoll Bounty]
+Complete [QC11 Riverpaw Gnoll Bounty][G26,88 Elwynn Forest].
+Hogger is still elite in TBC, if you're strong enough (unlikely) or can find someone nearby complete [QC176 Wanted:  "Hogger"]. Otherwise, abandon it.
+If you obtained *Gold Pickup Schedule*, accept [QA123 The Collector]. Otherwise skip this step.
+Turn in [QT11 Riverpaw Gnoll Bounty].\\Accept [QA109 Report to Gryan Stoutmantle][G24.2,74.6 Elwynn Forest].
+
+-- Westfall
+[A paladin]*Paladin*: You have to save 10 Linen Cloth for Lv.12.
+Turn in [QT184 Furlbrow's Deed].\\Accept [QA64 The Forgotten Heirloom].
+Accept [QA36 Westfall Stew] and [QA151 Poor Old Blanchy].
+Loot Sack of Oats on the way for [QC151 Poor Old Blanchy][O].
+Gather 3 [CI732,3 Okra] [G58,18 Westfall] here. If you run out of spawns, move on and gather the Okra on the way later.
+Grind to [XP10-630 630 xp from level 10].
+Deathwarp to Sentinel Hill.
+Turn in [QT109 Report to Gryan Stoutmantle].\\Accept [QA12 The People's Militia].
+Accept [QA102 Patrolling Westfall].
+Accept [QA6181 A Swift Message].
+Accept [QA153 Red Leather Bandanas].
+Get FP in [P Sentinel Hill].
+Turn in [QT6181 A Swift Message].\\Accept [QA6281 Continue to Stormwind].
+Fly to [F Stormwind].
+[O][T]Train Lv.10 skills while in Stormwind.
+Turn in [QT61 Shipment to Stormwind].
+Turn in [QT6281 Continue to Stormwind].\\Accept [QA6261 Dungar Longdrink].
+Turn in [QT6261 Dungar Longdrink].\\Accept [QA6285 Return to Lewis].
+Fly to [F Sentinel Hill].
+[O]We need 3 *Stringly Vulture Meat*, 3 *Goretusk Snout*. Also loot *Goretusk Liver*.
+Complete [QC12 The People's Militia][G47,38 Westfall]. There are mobs just outside of Sentinel Hill, as well as by Molsen's Farm and The Pumpkin Farm. Kill any *Goretusk* and *Fleshripper* you spot while doing so.
+Stand outside of the house [G49.319952,19.498831,1 Westfall] here, angle your camera to look into the house through the door and loot the wardrobe through the wall to complete [QC64 The Forgotten Heirloom].
+If you're close to finishing [QC153 Red Leather Bandanas], complete it. Otherwise skip and abandon.
+Complete [QC102 Patrolling Westfall][G45,14 Westfall].
+At the [G45,8 Westfall] beach, gather 3 [CI730,3 Murloc Eye].
+Try to finish up [QC151 Poor Old Blanchy][G53,29 Westfall] while finishing gathering 3 [CI729,3 Stringly Vulture Meat], 3 [CI731,3 Goretusk Snout]. If you're too far away, abandon the quest.
+If you are close to 8 [CI723,8 Goretusk Liver], gather those too.
+Turn in [QT64 The Forgotten Heirloom].
+Turn in [QT151 Poor Old Blanchy].
+Turn in [QT36 Westfall Stew].\\Accept follow-up [QA38 Westfall Stew].
+Turn in [QT38 Westfall Stew].
+If you got 8 livers, accept [QA22 Goretusk Liver Pie] and turn in [QT22 Goretusk Liver Pie]. Otherwise skip.
+Turn in [QT102 Patrolling Westfall].
+Turn in [QT12 The People's Militia].
+If you completed it, turn in [QT153 Red Leather Bandanas].
+
+-- Paladin
+[A paladin]*Paladin*: Make sure you get Lv.12 for Redemption quest.
+[A paladin]*Paladin*: Fly to Stormwind.
+[A paladin]*Paladin*: [T]Train skills.
+[A paladin]*Paladin*: From the *Tome of Dinvity*, accept [QA1642 The Tome of Divinity].
+[A paladin]*Paladin*: Turn in [QT1642 The Tome of Divinity].\\Accept follow-up [QA1643 The Tome of Divinity].
+[A paladin]*Paladin*: Turn in [QT1643 The Tome of Divinity].\\Accept follow-up [QA1644 The Tome of Divinity].
+[A paladin]*Paladin*: Turn in [QT1644 The Tome of Divinity].\\Accept follow-up [QA1780 The Tome of Divinity].
+[A paladin]*Paladin*: Turn in [QT1780 The Tome of Divinity].\\Accept follow-up [QA1781 The Tome of Divinity].
+[A paladin]*Paladin*: Turn in [QT1781 The Tome of Divinity].\\Accept follow-up [QA1786 The Tome of Divinity].
+[A paladin]*Paladin*: Set hearthstone in [S Stormwind City].
+[A paladin]*Paladin*: Turn in [QT176 Wanted:  "Hogger"].
+[A paladin]*Paladin*: Accept [QA1097 Elmore's Task].
+[A paladin]*Paladin*: Using the *Symbol of Life*, resurrect Henze to complete [QT1786 The Tome of Divinity].
+[A paladin]*Paladin*: Turn in [QT1786 The Tome of Divinity].\\Accept follow-up [QA1787 The Tome of Divinity].
+[A paladin]*Paladin*: Complete [QC1787 The Tome of Divinity].
+[A paladin]*Paladin*: Hearth to [H Stormwind City].
+[A paladin]*Paladin*: Turn in [QT1787 The Tome of Divinity].\\Accept follow-up [QA1788 The Tome of Divinity].
+[A paladin]*Paladin*: Turn in [QT1788 The Tome of Divinity].
+
+-- Goldshire
+[A paladin]*Paladin*: Ignore next two instructions.
+If you have Hogger as a turn-in or a class quest in Goldshire, hearth to [H Goldshire]. Otherwise, fly to Stormwind.
+Turn in [QT176 Wanted:  "Hogger"].
+Accept [QA1097 Elmore's Task].
+[T]Train skills.
+
+-- Stormwind
+Turn in [QT1097 Elmore's Task].
+Accept [QA6661 Deeprun Rat Roundup].
+In the tram area, complete [QC6661 Deeprun Rat Roundup].
+Turn in [QT6661 Deeprun Rat Roundup].
 
 XXX: Cooking
 XXX: Route to ironforge then menethil
